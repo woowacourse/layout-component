3e3fd9260f07ffd197909ead6330d99c537a504c
Gilpop8663 <wolfye@naver.com>
2023-09-15 17:47:47 +0900

    feat: Conatiner에 children을 받도록 구현 및 예시 코드 구현

--- src/Container/index.tsx
+++ src/Container/index.tsx
@@ -1,4 +1,6 @@
-interface ContainerProps {
+import { PropsWithChildren } from 'react';
+
+interface ContainerProps extends PropsWithChildren {
   minWidth?: number;
   maxWidth?: number;
   paddingX?: number;
@@ -14,15 +16,19 @@ export default function Container({
   paddingY = 0,
   marginX = 0,
   marginY = 0,
+  children,
 }: ContainerProps) {
   return (
     <div
       style={{
+        width: '100vw',
         minWidth: minWidth ? `${minWidth}px` : '',
         maxWidth: maxWidth ? `${maxWidth}px` : '',
         padding: `${paddingY}px ${paddingX}px`,
         margin: `${marginY}px ${marginX}px`,
       }}
-    ></div>
+    >
+      {children}
+    </div>
   );
 }
