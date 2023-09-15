b0ee73d76313a37e086437982fd3326eccc55a59
dahyeee <06robin11@gmail.com>
2023-09-15 13:30:43 +0900

    refactor: Container 프롭스명 수정

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,7 +1,13 @@
-import type { ComponentPropsWithoutRef } from 'react';
+import type { ComponentPropsWithoutRef, ElementType } from 'react';
 import { wrapperStyle } from './Container.style';
 
 interface ContainerProps extends ComponentPropsWithoutRef<React.ElementType> {
+  /**
+   * Container의 HTML 태그
+   *
+   * @default 'div'
+   */
+  as?: ElementType;
   /**
    * 최대 넓이 (number로 입력시 px로 고정, string으로 입력시 그대로 반영)
    *
@@ -14,25 +20,20 @@ interface ContainerProps extends ComponentPropsWithoutRef<React.ElementType> {
    * @default '100px'
    */
   minWidth?: number | string;
-  /**
-   * Container의 tag로 설정하고 싶은 값
-   *
-   * @default 'div'
-   */
-  as?: 'div' | 'dialog' | 'section' | 'article' | 'area' | 'form';
 }
 
 const Container = ({
   maxWidth = '100%',
   minWidth = '100px',
-  as: RenderTag = 'div',
+  as = 'div',
   children,
   ...attributes
 }: ContainerProps) => {
+  const Tag = as;
   return (
-    <RenderTag {...attributes} css={wrapperStyle(minWidth, maxWidth)}>
+    <Tag {...attributes} css={wrapperStyle(minWidth, maxWidth)}>
       {children}
-    </RenderTag>
+    </Tag>
   );
 };
 
