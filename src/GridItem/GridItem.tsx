845626b6b4de583c9d4edf858c57498d7f09a360
hae-on <solbi2004@naver.com>
2023-09-16 17:17:29 +0900

    feat: row, col과 관련된 기능 추가

--- src/GridItem/GridItem.tsx
+++ src/GridItem/GridItem.tsx
@@ -2,6 +2,12 @@ import type { ComponentPropsWithoutRef, ReactNode } from 'react';
 
 interface GridItemProps extends ComponentPropsWithoutRef<'div'> {
   area?: string;
+  row?: number | 'auto';
+  col?: number | 'auto';
+  rowStart?: number | 'auto';
+  rowEnd?: number | 'auto';
+  colStart?: number | 'auto';
+  colEnd?: number | 'auto';
   width?: string;
   height?: string;
   bgColor?: string;
@@ -10,13 +16,25 @@ interface GridItemProps extends ComponentPropsWithoutRef<'div'> {
 
 const GridItem = ({
   area,
+  row,
+  col,
+  rowStart,
+  rowEnd,
+  colStart,
+  colEnd,
   width,
   height,
   bgColor,
   children,
 }: GridItemProps) => {
   const itemStyles = {
-    gridArea: area,
+    gridArea: area ?? area,
+    gridColumn: col === 'auto' ? 'auto' : `span ${col} / span ${col}`,
+    gridRow: row === 'auto' ? 'auto' : `span ${row} / span ${row}`,
+    gridColumnStart: colStart,
+    gridColumnEnd: colEnd,
+    gridRowStart: rowStart,
+    gridRowEnd: rowEnd,
     width: width,
     height: height,
     backgroundColor: bgColor,
