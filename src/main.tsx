9aa02c3f56ec9e021926cc4322ea7bb8812a4c04
Dain Lee <feb.dain@gmail.com>
2023-09-17 01:42:25 +0900

    design: reset css 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
+import { Global } from '@emotion/react';
+
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { reset } from '@styles/reset';
+
 import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Global styles={reset} />
     <App />
   </StrictMode>
 );
