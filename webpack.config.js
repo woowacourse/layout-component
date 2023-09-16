8a5ed46b6ddc8e2ef02d18834937b1c221557c0d
TaeeunKim <xodms0309@naver.com>
2023-09-17 00:28:24 +0900

    chore: copy-webpack plugin 설치

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.ts',
@@ -24,4 +25,14 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'src/**/*.css',
+          to: '[name][ext]',
+        },
+      ],
+    }),
+  ],
 };
