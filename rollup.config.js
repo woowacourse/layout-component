c8c71ee88532fe059559d6fc30cb61d5c4e4956b
Leejin-Yang <ejin1996@gmail.com>
2023-09-16 17:28:45 +0900

    chore: 프로젝트 환경 설정

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+
+const extensions = ['.js', '.jsx', '.ts', '.tsx'];
+
+const inputSrc = [
+  ['./src/index.ts', 'cjs'],
+  ['./src/index.ts', 'esm'],
+];
+
+export default inputSrc.map(([input, format]) => ({
+  input,
+  output: {
+    dir: `dist/${format}`,
+    format,
+  },
+  preserveModules: format === 'cjs',
+  plugins: [
+    nodeResolve({ extensions }),
+    commonjs(),
+    babel({
+      babelHelpers: 'runtime',
+      extensions,
+      exclude: 'node_modules/**',
+      presets: [
+        '@babel/preset-env',
+        '@babel/preset-react',
+        '@babel/preset-typescript',
+      ],
+      plugins: ['@babel/plugin-transform-runtime'],
+    }),
+    terser(),
+  ],
+}));
