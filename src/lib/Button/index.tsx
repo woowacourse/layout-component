e98fc1c415719ffff2744361894eb1fe5c35ac33
inyeong-kang <tomatorkd@naver.com>
2023-09-17 00:10:28 +0900

    refactor: css 모듈을 emotion 스타일링으로 수정, App.tsx 불필요한 코드 삭제

--- src/lib/Button/index.tsx
+++ src/lib/Button/index.tsx
@@ -1,8 +1,8 @@
-import styles from './Button.module.css';
+import styled from '@emotion/styled';
+import { HTMLAttributes } from 'react';
 
-interface ButtonProps {
+interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
   primary?: boolean;
-  backgroundColor?: string;
   size?: 'sm' | 'md' | 'lg';
   label: string;
   onClick?: () => void;
@@ -11,23 +11,51 @@ interface ButtonProps {
 export default function Button({
   primary = false,
   size = 'md',
-  backgroundColor,
   label,
   ...props
 }: ButtonProps) {
-  const mode = primary
-    ? styles['button--primary']
-    : styles['button--secondary'];
-  const sizeClass = styles[`button--${size}`];
-
   return (
-    <button
-      type='button'
-      className={[styles['button'], sizeClass, mode].join(' ')}
-      style={{ backgroundColor }}
-      {...props}
-    >
+    <StyledButton type='button' $primary={primary} $size={size} {...props}>
       {label}
-    </button>
+    </StyledButton>
   );
 }
+
+const SIZE = {
+  sm: {
+    fontSize: '12px',
+    padding: '10px 16px',
+  },
+  md: {
+    fontSize: '14px',
+    padding: '11px 20px',
+  },
+  lg: {
+    fontSize: '16px',
+    padding: '12px 24px',
+  },
+};
+
+const StyledButton = styled.button<{
+  $primary: boolean;
+  $size: 'sm' | 'md' | 'lg';
+}>`
+  display: inline-block;
+
+  padding: ${(props) => SIZE[props.$size].padding};
+  border-radius: 3em;
+  border: ${(props) =>
+    props.$primary ? '1px solid #1ea7fd' : '1px solid gray'};
+
+  color: ${(props) => (props.$primary ? 'white' : 'black')};
+  background-color: ${(props) => (props.$primary ? '#1ea7fd' : 'transparent')};
+
+  box-shadow: ${(props) =>
+    props.$primary && ' rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset'};
+
+  font-size: ${(props) => SIZE[props.$size].fontSize};
+  font-weight: 700;
+  line-height: 1;
+
+  cursor: pointer;
+`;
