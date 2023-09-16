f62fab1f01fb4154f6f7876856f6796d7ccd34ce
inyeong-kang <tomatorkd@naver.com>
2023-09-16 22:12:07 +0900

    feat: Grid 컴포넌트 구현

---
+++ src/components/Grid/index.tsx
@@ -0,0 +1,49 @@
+import { HTMLAttributes } from 'react';
+import styled from '@emotion/styled';
+
+interface GridProps extends HTMLAttributes<HTMLDivElement> {
+  backgroundColor?: string;
+  rows: number;
+  columns: number;
+  gap?: string;
+  children: React.ReactNode;
+}
+
+export default function Grid({
+  backgroundColor,
+  rows,
+  columns,
+  gap,
+  children,
+  ...props
+}: GridProps) {
+  return (
+    <StyledGrid
+      $rows={rows}
+      $columns={columns}
+      $gap={gap ?? '0'}
+      $backgroundColor={backgroundColor ?? 'white'}
+      {...props}
+    >
+      {children}
+    </StyledGrid>
+  );
+}
+
+const StyledGrid = styled.div<{
+  $rows: number;
+  $columns: number;
+  $gap: string;
+  $backgroundColor: string;
+}>`
+  display: grid;
+  grid-template-rows: ${(props) => `repeat(${props.$rows}, 1fr)`};
+  grid-template-columns: ${(props) => `repeat(${props.$columns}, 1fr)`};
+  gap: ${(props) => props.$gap};
+
+  height: 90vh;
+  padding: 10px;
+
+  color: gray;
+  background-color: ${(props) => props.$backgroundColor};
+`;
