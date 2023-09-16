1ad410319be4aee82e6870133ab9cf396e47e39d
hae-on <solbi2004@naver.com>
2023-09-16 22:50:00 +0900

    docs: JSDOCS 추가

--- src/Grid/Grid.tsx
+++ src/Grid/Grid.tsx
@@ -1,15 +1,41 @@
 import type { ComponentPropsWithoutRef, ReactNode } from 'react';
 
 interface GridProps extends ComponentPropsWithoutRef<'div'> {
+  /**
+   * Grid의 영역을 정의합니다.
+   */
   areas?: string;
+  /**
+   * Grid의 행 수를 지정합니다.
+   */
   rows?: number;
+  /**
+   * Grid의 열 수를 지정합니다.
+   */
   columns?: number;
+  /**
+   * Grid Item 사이의 간격을 지정합니다.
+   */
   gap?: number;
+  /**
+   * Grid의 높이를 지정합니다.
+   */
   height?: string;
+  /**
+   * Grid의 자식 컴포넌트입니다.
+   */
   children: ReactNode[];
 }
 
-const Grid = ({ areas, rows, columns, gap, height, children }: GridProps) => {
+const Grid = ({
+  areas,
+  rows,
+  columns,
+  gap,
+  height,
+  children,
+  ...props
+}: GridProps) => {
   const gridStyles = {
     display: 'grid',
     gridTemplateAreas: areas,
@@ -19,7 +45,11 @@ const Grid = ({ areas, rows, columns, gap, height, children }: GridProps) => {
     height: height,
   };
 
-  return <div style={gridStyles}>{children}</div>;
+  return (
+    <div style={gridStyles} {...props}>
+      {children}
+    </div>
+  );
 };
 
 export default Grid;
--- src/GridItem/GridItem.tsx
+++ src/GridItem/GridItem.tsx
@@ -1,16 +1,49 @@
 import type { ComponentPropsWithoutRef, ReactNode } from 'react';
 
 interface GridItemProps extends ComponentPropsWithoutRef<'div'> {
+  /**
+   * GridItem의 영역을 정의합니다.
+   */
   area?: string;
+  /**
+   * GridItem의 행 수를 지정합니다.
+   */
   row?: number | 'auto';
+  /**
+   * GridItem의 열 수를 지정합니다.
+   */
   col?: number | 'auto';
+  /**
+   * GridItem의 행이 시작하는 위치를 지정합니다.
+   */
   rowStart?: number | 'auto';
+  /**
+   * GridItem의 행이 끝나는 위치를 지정합니다.
+   */
   rowEnd?: number | 'auto';
+  /**
+   * GridItem의 열이 시작하는 위치를 지정합니다.
+   */
   colStart?: number | 'auto';
+  /**
+   * GridItem의 열이 끝나는 위치를 지정합니다.
+   */
   colEnd?: number | 'auto';
+  /**
+   * GridItem의 너비를 지정합니다.
+   */
   width?: string;
+  /**
+   * GridItem의 높이를 지정합니다.
+   */
   height?: string;
+  /**
+   * GridItem의 배경 색상입니다.
+   */
   bgColor?: string;
+  /**
+   * GridItem의 배경 색상입니다.
+   */
   children?: ReactNode;
 }
 
