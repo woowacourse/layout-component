d98b0a9bbb80dc77861e103414d05d1c9df28b5a
TaeeunKim <xodms0309@naver.com>
2023-09-16 22:45:43 +0900

    chore: style-loader, css-loader 설치

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,10 @@ module.exports = {
         exclude: /node_modules/,
         use: 'ts-loader',
       },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
 };
