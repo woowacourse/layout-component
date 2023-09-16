35a2ff68194d98b7cad2df00ab347d2adebf4694
Dain Lee <feb.dain@gmail.com>
2023-09-17 05:21:49 +0900

    design: font 설정

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,14 @@ import { Global } from '@emotion/react';
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { common } from '@styles/common.ts';
 import { reset } from '@styles/reset';
 
 import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Global styles={reset} />
+    <Global styles={[reset, common]} />
     <App />
   </StrictMode>
 );
