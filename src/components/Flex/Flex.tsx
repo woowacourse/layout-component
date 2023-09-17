0cfaab0454eb80b8f381021a91bc3c5f58f67317
Suyoung Jung <jsy19970512@naver.com>
2023-09-18 03:23:45 +0900

    feat: Flex 컴포넌트 작성

---
+++ src/components/Flex/Flex.tsx
@@ -0,0 +1,42 @@
+import type { ContentSectioningTags } from '~/types';
+import * as S from './Flex.styled';
+
+export interface FlexProps {
+  children: React.ReactNode;
+  as?: ContentSectioningTags;
+  direction?: React.CSSProperties['flexDirection'];
+  justify?: React.CSSProperties['justifyContent'];
+  align?: React.CSSProperties['alignItems'];
+  wrap?: React.CSSProperties['flexWrap'];
+  gap?: React.CSSProperties['gap'];
+  css?: React.CSSProperties;
+}
+
+const Flex = (props: FlexProps) => {
+  const {
+    children,
+    as = 'div',
+    direction = 'row',
+    justify = 'flex-start',
+    align = 'flex-start',
+    wrap = 'nowrap',
+    gap = 0,
+    css,
+  } = props;
+
+  return (
+    <S.FlexRoot
+      as={as}
+      direction={direction}
+      justify={justify}
+      align={align}
+      wrap={wrap}
+      gap={gap}
+      css={css}
+    >
+      {children}
+    </S.FlexRoot>
+  );
+};
+
+export default Flex;
