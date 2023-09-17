b62e11601f0642933fcb2df6007155fbd909e6c4
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-09-17 23:42:59 +0900

    chore: npm 배포 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve('src', 'lib/index.ts'),
+      name: 'react-aaker-layout-components',
+      fileName: (format) => `react-aaker-layout-components.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+        },
+      },
+    },
+  },
   plugins: [react()],
-})
+});
