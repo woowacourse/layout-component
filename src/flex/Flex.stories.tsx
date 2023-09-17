8ccd24f0cfb376e9b48f1e34effb3a0e17b379bb
chsua <csj1919@naver.com>
2023-09-17 22:17:22 +0900

    test: container, flex, grid 컴포넌트 예시본 작성

---
+++ src/flex/Flex.stories.tsx
@@ -0,0 +1,45 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import React from "react";
+import Flex from ".";
+
+const meta: Meta<typeof Flex> = {
+  component: Flex,
+};
+
+export default meta;
+type Story = StoryObj<typeof Flex>;
+
+export const Column: Story = {
+  render: () => (
+    <Flex direction="column" gap={10}>
+      {new Array(5).fill(0).map((_) => {
+        return (
+          <div
+            style={{
+              backgroundColor: "royalblue",
+              height: "100px",
+            }}
+          />
+        );
+      })}
+    </Flex>
+  ),
+};
+
+export const Rows: Story = {
+  render: () => (
+    <Flex direction="row" justify="flex-end" gap={10} flexWrap="wrap">
+      {new Array(10).fill(0).map((_) => {
+        return (
+          <div
+            style={{
+              backgroundColor: "royalblue",
+              height: "100px",
+              width: "100px",
+            }}
+          />
+        );
+      })}
+    </Flex>
+  ),
+};
