fb4408c17f32d5b3b41cd5ba95fe91bfd586dc26
hae-on <solbi2004@naver.com>
2023-09-17 16:22:42 +0900

    feat: container 최소, 최대 너비와 배경 색상 props 추가

--- src/Container/Container.tsx
+++ src/Container/Container.tsx
@@ -1,11 +1,37 @@
 import { ComponentPropsWithoutRef, ReactNode } from 'react';
 
 interface ContainerProps extends ComponentPropsWithoutRef<'div'> {
+  /**
+   * Container의 최소 너비입니다.
+   */
+  minWidth?: string;
+  /**
+   * Container의 최대 너비입니다.
+   * @default '100%'
+   */
+  maxWidth?: string;
+  /**
+   * Container의 배경 색상입니다.
+   */
+  bgColor?: string;
+  /**
+   * Container의 자식 컴포넌트입니다.
+   */
   children: ReactNode;
 }
 
-const Container = ({ children, ...props }: ContainerProps) => {
-  const containerStyles = {};
+const Container = ({
+  minWidth,
+  maxWidth,
+  bgColor,
+  children,
+  ...props
+}: ContainerProps) => {
+  const containerStyles = {
+    minWidth: minWidth,
+    maxWidth: maxWidth,
+    backgroundColor: bgColor,
+  };
 
   return (
     <div style={containerStyles} {...props}>
