aaec8549a2a04623e0f956676c13967ab9b346db
kyw0716 <kyw0716@naver.com>
2023-09-17 19:41:02 +0900

    refactor: Grid 컴포넌트에 css 속성 추가

--- src/common/Grid/Grid.tsx
+++ src/common/Grid/Grid.tsx
@@ -1,19 +1,20 @@
 import styled from 'styled-components';
 import { GridLayoutProps, Props } from './type';
 
-const Grid = ({ row, column, gap, children }: Props) => {
+const Grid = ({ row, column, gap, css, children }: Props) => {
   return (
-    <GridLayout $row={row} $column={column} $gap={gap}>
+    <GridLayout $row={row} $column={column} $gap={gap} $css={css}>
       {children}
     </GridLayout>
   );
 };
 
 const GridLayout = styled.div<GridLayoutProps>`
+  ${({ $css }) => $css}
+
   width: 100%;
 
   display: grid;
-
   grid-template-columns: repeat(${({ $column }) => $column}, 1fr);
   grid-template-rows: repeat(${({ $row }) => $row}, 1fr);
   gap: ${({ $gap }) => ($gap ? $gap : 0)}px;
--- src/common/Grid/type.ts
+++ src/common/Grid/type.ts
@@ -1,13 +1,16 @@
 import { PropsWithChildren } from 'react';
+import { CSSProp } from 'styled-components';
 
 export interface Props extends PropsWithChildren {
   row: number;
   column: number;
   gap?: number;
+  css?: CSSProp;
 }
 
 export interface GridLayoutProps {
   $row: number;
   $column: number;
   $gap?: number;
+  $css?: CSSProp;
 }
