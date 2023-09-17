d29b3ceb7befe6cf5e0b0dec5df7c310f040f321
chex <chex1004@gmail.com>
2023-09-17 17:05:59 +0900

    docs: JSDocs 추가

--- src/components/@common/Container/Container.tsx
+++ src/components/@common/Container/Container.tsx
@@ -2,10 +2,15 @@ import { ComponentPropsWithRef, ElementType } from 'react';
 import { CSSProp, styled } from 'styled-components';
 
 interface Props extends ComponentPropsWithRef<ElementType> {
+  /** 컴포넌트의 HTML 태그를 지정하는 속성입니다. 예를 들어, tag="div"로 설정하면 컴포넌트가 <div> 태그로 렌더링됩니다. */
   tag?: keyof JSX.IntrinsicElements;
-  minWidth?: number;
-  maxWidth?: number;
+  /** 컴포넌트의 최소 너비를 지정하는 속성입니다. */
+  minWidth?: string;
+  /** 컴포넌트의 최대 너비를 지정하는 속성입니다. */
+  maxWidth?: string;
+  /** 컴포넌트의 배경색상을 지정하는 속성입니다. */
   backgroundColor?: string;
+  /** 컨테이너에 적용할 CSS 스타일을 설정하는 속성입니다. */
   css?: CSSProp;
 }
 
@@ -27,12 +32,12 @@ const Tag = styled.div<Props>`
   justify-content: center;
   align-items: center;
 
-  width: ${({ width }) => (width ? `${width}px` : 'auto')};
-  height: ${({ height }) => (height ? `${height}px` : 'auto')};
+  width: ${({ width }) => (width ? `${width}` : 'auto')};
+  height: ${({ height }) => (height ? `${height}` : 'auto')};
   background-color: ${({ backgroundColor }) => (backgroundColor ? `${backgroundColor}` : '#fff')};
 
-  min-width: ${({ minWidth }) => (minWidth ? `${minWidth}px` : 'auto')};
-  max-width: ${({ maxWidth }) => (maxWidth ? `${maxWidth}px` : 'none')};
+  min-width: ${({ minWidth }) => (minWidth ? `${minWidth}` : 'auto')};
+  max-width: ${({ maxWidth }) => (maxWidth ? `${maxWidth}` : 'none')};
 
   ${({ css }) => (css ? `${css}` : '')};
 `;
