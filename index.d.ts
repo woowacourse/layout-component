0d7580aef371ca60322ccbaa2dcab9458d8605a2
Q Kim <qoli0703@gmail.com>
2023-09-17 17:31:59 +0900

    refactor: 타입 정리
    MDN 문서의 formal syntax와 비슷하게 정리

--- index.d.ts
+++ index.d.ts
@@ -1,47 +1,59 @@
-declare type PositionalAlignment = 'center' | 'start' | 'end' | 'flex-start' | 'flex-end';
-declare type AlignItemsPositionalAlignment = PositionalAlignment | 'self-start' | 'self-end';
-declare type JustifyContentPositionalAlignment = PositionalAlignment | 'left' | 'right';
+declare type OverflowPosition = 'unsafe' | 'safe';
 
-declare interface ContainerProps extends React.HTMLAttributes<HTMLDivElement> {
+declare type BaselinePosition =
+  | 'baseline'
+  | 'first baseline'
+  | 'last baseline'
+  | 'baseline first'
+  | 'baseline last';
+declare type SelfPosition =
+  | 'center'
+  | 'start'
+  | 'end'
+  | 'self-start'
+  | 'self-end'
+  | 'flex-start'
+  | 'flex-end';
+export declare type AlignItems =
+  | 'normal'
+  | 'stretch'
+  | BaselinePosition
+  | SelfPosition
+  | `${OverflowPosition} ${SelfPosition}`;
+
+declare type ContentDistribution = 'space-between' | 'space-evenly' | 'space-around' | 'stretch';
+declare type ContentPosition = 'center' | 'start' | 'end' | 'flex-start' | 'flex-end';
+declare type Direction = 'left' | 'right';
+export declare type JustifyContent =
+  | 'normal'
+  | ContentDistribution
+  | Direction
+  | ContentPosition
+  | `${OverflowPosition} ${ContentPosition}`
+  | `${OverflowPosition} ${Direction}`;
+
+export declare interface ContainerProps extends React.HTMLAttributes<HTMLDivElement> {
   minWidth?: string;
   maxWidth?: string;
 }
 
-declare interface FlexProps extends React.HTMLAttributes<HTMLDivElement> {
+export declare interface FlexProps extends React.HTMLAttributes<HTMLDivElement> {
   direction?: 'row' | 'row-reverse' | 'column' | 'column-reverse';
-  justify?:
-    | JustifyContentPositionalAlignment
-    | `safe ${JustifyContentPositionalAlignment}`
-    | `unsafe ${JustifyContentPositionalAlignment}`
-    | 'normal'
-    | 'space-between'
-    | 'space-evenly'
-    | 'space-around'
-    | 'stretch';
-  align?:
-    | AlignItemsPositionalAlignment
-    | `first ${AlignItemsPositionalAlignment}`
-    | `last ${AlignItemsPositionalAlignment}`
-    | `safe ${AlignItemsPositionalAlignment}`
-    | `unsafe ${AlignItemsPositionalAlignment}`
-    | 'normal'
-    | 'stretch'
-    | 'baseline';
+  justify?: JustifyContent;
+  align?: AlignItems;
   wrap?: 'wrap' | 'nowrap' | 'wrap-reverse';
   gap?: string;
   inline?: boolean;
 }
 
-declare interface GridProps extends React.HTMLAttributes<HTMLDivElement> {
+export declare interface GridProps extends React.HTMLAttributes<HTMLDivElement> {
   rows: number;
   columns: number;
   gap?: string;
 }
 
-declare const LayoutComponent: {
-  Container: (props: React.PropsWithChildren<ContainerProps>) => JSX.Element;
-  Flex: (props: React.PropsWithChildren<FlexProps>) => JSX.Element;
-  Grid: (props: React.PropsWithChildren<GridProps>) => JSX.Element;
-};
+declare const Container: (props: React.PropsWithChildren<ContainerProps>) => JSX.Element;
+declare const Flex: (props: React.PropsWithChildren<FlexProps>) => JSX.Element;
+declare const Grid: (props: React.PropsWithChildren<GridProps>) => JSX.Element;
 
-export default LayoutComponent;
+export { Container, Flex, Grid };
--- src/Flex/index.tsx
+++ src/Flex/index.tsx
@@ -9,30 +9,44 @@ import {
   flexWrap,
 } from './Flex.css';
 
-type PositionalAlignment = 'center' | 'start' | 'end' | 'flex-start' | 'flex-end';
-type JustifyContentPositionalAlignment = PositionalAlignment | 'left' | 'right';
-type AlignItemsPositionalAlignment = PositionalAlignment | 'self-start' | 'self-end';
+type OverflowPosition = 'unsafe' | 'safe';
+
+type BaselinePosition =
+  | 'baseline'
+  | 'first baseline'
+  | 'last baseline'
+  | 'baseline first'
+  | 'baseline last';
+type SelfPosition =
+  | 'center'
+  | 'start'
+  | 'end'
+  | 'self-start'
+  | 'self-end'
+  | 'flex-start'
+  | 'flex-end';
+type AlignItems =
+  | 'normal'
+  | 'stretch'
+  | BaselinePosition
+  | SelfPosition
+  | `${OverflowPosition} ${SelfPosition}`;
+
+type ContentDistribution = 'space-between' | 'space-evenly' | 'space-around' | 'stretch';
+type ContentPosition = 'center' | 'start' | 'end' | 'flex-start' | 'flex-end';
+type Direction = 'left' | 'right';
+type JustifyContent =
+  | 'normal'
+  | ContentDistribution
+  | Direction
+  | ContentPosition
+  | `${OverflowPosition} ${ContentPosition}`
+  | `${OverflowPosition} ${Direction}`;
 
 interface FlexProps extends React.HTMLAttributes<HTMLDivElement> {
   direction?: 'row' | 'row-reverse' | 'column' | 'column-reverse';
-  justify?:
-    | JustifyContentPositionalAlignment
-    | `safe ${JustifyContentPositionalAlignment}`
-    | `unsafe ${JustifyContentPositionalAlignment}`
-    | 'normal'
-    | 'space-between'
-    | 'space-evenly'
-    | 'space-around'
-    | 'stretch';
-  align?:
-    | AlignItemsPositionalAlignment
-    | `first ${AlignItemsPositionalAlignment}`
-    | `last ${AlignItemsPositionalAlignment}`
-    | `safe ${AlignItemsPositionalAlignment}`
-    | `unsafe ${AlignItemsPositionalAlignment}`
-    | 'normal'
-    | 'stretch'
-    | 'baseline';
+  justify?: JustifyContent;
+  align?: AlignItems;
   wrap?: 'wrap' | 'nowrap' | 'wrap-reverse';
   gap?: string;
   inline?: boolean;
