95564975dbee443a9d44b69af11982d729c5b95b
jeongwusi <jeongwusi@gmail.com>
2023-09-17 16:42:28 +0900

    ✨ feat: Flex 컴포넌트 구현

---
+++ src/Flex/Flex.tsx
@@ -0,0 +1,26 @@
+interface FlexProps {
+  children: React.ReactNode;
+  direction: "row" | "column";
+  justify:
+    | "flex-start"
+    | "flex-end"
+    | "center"
+    | "space-between"
+    | "space-around";
+  align: "flex-start" | "flex-end" | "center" | "stretch";
+  gap: number;
+}
+
+const Flex = ({ children, direction, justify, align, gap }: FlexProps) => {
+  const flexStyle: React.CSSProperties = {
+    display: "flex",
+    flexDirection: direction,
+    justifyContent: justify,
+    alignItems: align,
+    gap: `${gap}px`,
+  };
+
+  return <div style={flexStyle}>{children}</div>;
+};
+
+export default Flex;
