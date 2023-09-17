f08112e50ad062eab5ebd2c6bce76994ba1234c4
sh981013s <sh981013s@gmail.com>
2023-09-18 02:16:39 +0900

    test: stroybook 테스트 mdx 형식으로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,17 @@ import { Container, Flex, Grid } from './lib';
 const App = () => {
   return (
     <>
-      <Container />
-      <Flex />
-      <Grid />
+      <Container maxWidth={100}>hi</Container>
+      <Flex gap={10}>
+        <div>first</div>
+        <div>second</div>
+        <div>third</div>
+      </Flex>
+      <Grid gap={10} rows={1} columns={2}>
+        <div>first</div>
+        <div>second</div>
+        <div>third</div>
+      </Grid>
     </>
   );
 };
