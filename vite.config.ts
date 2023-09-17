14b2ffe0a397036fcbbbbc379ff717cc059a0a6b
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-09-18 00:09:06 +0900

    fix: `Container` 컴포넌트 타입 props 되도록 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import dts from 'vite-plugin-dts';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/lib/index.ts'),
       name: 'react-aaker-layout-components',
-      fileName: (format) => `react-aaker-layout-components.${format}.js`,
+      formats: ['es'],
+      fileName: 'react-aaker-layout-components',
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
