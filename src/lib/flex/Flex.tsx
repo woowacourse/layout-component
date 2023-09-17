6fecf676ce1af5857312de9e10eb10b76bce94c8
sh981013s <sh981013s@gmail.com>
2023-09-18 01:22:24 +0900

    feat: Flex UI 컴포넌트 구현

--- src/lib/flex/Flex.tsx
+++ src/lib/flex/Flex.tsx
@@ -1,5 +1,29 @@
-const Flex = () => {
-  return <div>Flex</div>;
+import { FlexProps } from './types.ts';
+import { useEffect, useState } from 'react';
+import * as S from './Flex.styles.tsx';
+
+const Flex = (props: FlexProps) => {
+  const { direction, justify, align, gap, children } = props;
+
+  const [doesBrowserSupportGap, setDoesBrowserSupportGap] = useState(false);
+
+  useEffect(() => {
+    setDoesBrowserSupportGap(
+      window.CSS && window.CSS.supports && window.CSS.supports('gap', '0px'),
+    );
+  }, []);
+
+  return (
+    <S.Flex
+      direction={direction}
+      justify={justify}
+      align={align}
+      gap={gap}
+      doesBrowserSupportGap={doesBrowserSupportGap}
+    >
+      {children}
+    </S.Flex>
+  );
 };
 
 export default Flex;
