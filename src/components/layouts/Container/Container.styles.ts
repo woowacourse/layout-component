3a0608c83c9e5fb59e19a3472f8bee98a6a2c5cd
hozzijeong <ghwls0905@naver.com>
2023-09-17 16:44:59 +0900

    feat: Container 컵포넌트 생성

--- src/components/layouts/Container/Container.styles.ts
+++ src/components/layouts/Container/Container.styles.ts
@@ -1,1 +1,19 @@
+import styled from '@emotion/styled';
+import { CSSObject, css } from '@emotion/react';
+interface WrapperProps {
+	minWidth: number;
+	maxWidth: number;
+	customCss?: CSSObject;
+}
 
+export const Wrapper = styled.div<WrapperProps>`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+
+	width: 100%;
+	min-width: ${(props) => props.minWidth}px;
+	max-width: ${(props) => props.maxWidth}px;
+
+	${({ customCss }) => customCss && css({ ...customCss })};
+`;
--- src/components/layouts/Container/index.tsx
+++ src/components/layouts/Container/index.tsx
@@ -1,5 +1,30 @@
-const Container = () => {
-	return <div></div>;
+import { CSSObject } from '@emotion/react';
+import { Wrapper } from './Container.styles';
+
+type TagType = Pick<
+	HTMLElementTagNameMap,
+	'div' | 'section' | 'main' | 'header'
+>;
+
+interface ContainerProps extends React.PropsWithChildren {
+	tag?: keyof TagType;
+	minWidth: number;
+	maxWidth: number;
+	customCss?: CSSObject;
+}
+
+const Container = (props: ContainerProps) => {
+	const { tag = 'div', minWidth, maxWidth, customCss, children } = props;
+	return (
+		<Wrapper
+			as={tag}
+			minWidth={minWidth}
+			maxWidth={maxWidth}
+			customCss={customCss}
+		>
+			{children}
+		</Wrapper>
+	);
 };
 
 export default Container;
