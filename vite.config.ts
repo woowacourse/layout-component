d0217ce2e4d083f1efb3fd305cac8132c6c42636
chex <chex1004@gmail.com>
2023-09-17 18:00:57 +0900

    chore: 빌드 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/lib/index.tsx'),
+      name: 'index',
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        globals: {
+          react: 'React',
+        },
+      },
+    },
+    commonjsOptions: {
+      esmExternals: ['react'],
+    },
+  },
+  plugins: [react(), dts()],
   base: '/layout-component/',
 });
