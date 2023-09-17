e0d984511dc737a22b27046d7aa733cf98a96d2f
jeongwusi <jeongwusi@gmail.com>
2023-09-17 16:42:46 +0900

    ✨ feat: Grid 컴포넌트 구현

---
+++ src/Grid/Grid.tsx
@@ -0,0 +1,19 @@
+interface GridProps {
+  children: React.ReactNode;
+  rows: number;
+  columns: number;
+  gap: number;
+}
+
+const Grid = ({ children, rows, columns, gap }: GridProps) => {
+  const gridStyle: React.CSSProperties = {
+    display: "grid",
+    gridTemplateRows: `repeat(${rows}, 1fr)`,
+    gridTemplateColumns: `repeat(${columns}, 1fr)`,
+    gap: `${gap}px`,
+  };
+
+  return <div style={gridStyle}>{children}</div>;
+};
+
+export default Grid;
