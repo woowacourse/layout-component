ef7fa6becfa0c083bd153758a362f77a5ce26a7c
GC-Park <kuyt3122@naver.com>
2023-09-17 16:03:38 +0900

    feat: theme 설정

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {ThemeProvider} from "styled-components";
+import theme from './theme/index.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+    
   </React.StrictMode>,
 )
---
+++ src/theme/index.ts
@@ -0,0 +1,11 @@
+import color from './color'
+import radius from './radius'
+import space from './space'
+
+const theme = {
+  color,
+  radius,
+  space,
+} as const
+
+export default theme
