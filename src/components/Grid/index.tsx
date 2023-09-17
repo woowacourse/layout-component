69908f664ddfb638592e3eaee776f605e93ba013
Jungwoo <evencoding@gmail.com>
2023-09-17 19:10:06 +0900

    feat(Grid): 고유의 스타일 prop으로 inline style을 생성하도록 구현

--- src/components/Grid/index.tsx
+++ src/components/Grid/index.tsx
@@ -1,15 +1,32 @@
-import { HTMLProps } from 'react';
+import { CSSProperties, HTMLProps } from 'react';
 import type { CommonProps } from '../../types';
+import type { Gap } from '../../types/components/common';
+import type { Columns, Rows } from '../../types/components/grid';
 import { parseClassName } from '../../utils/parseClassName';
 import styles from './Grid.module.css';
 
-interface GridProps extends CommonProps, HTMLProps<HTMLDivElement> {}
+interface GridProps extends CommonProps, HTMLProps<HTMLDivElement> {
+  rows: Rows;
+  columns: Columns;
+  gap?: Gap;
+}
 
-const Grid = ({ sx, children, className = '', ...rest }: GridProps) => {
+const Grid = ({ rows, columns, gap, sx, children, className = '', ...rest }: GridProps) => {
   const { dynamicStyle, staticClassName } = parseClassName(className, styles);
 
+  const gridInlineStyle: CSSProperties = {
+    display: 'grid',
+    ...(rows && { gridTemplateRows: `repeat(${rows}, 1fr)` }),
+    ...(columns && { gridTemplateColumns: `repeat(${columns}, 1fr)` }),
+    ...(gap && { gap }),
+  };
+
   return (
-    <div style={{ ...sx, ...dynamicStyle }} className={staticClassName} {...rest}>
+    <div
+      style={{ ...gridInlineStyle, ...dynamicStyle, ...sx }}
+      className={`${staticClassName}`}
+      {...rest}
+    >
       {children}
     </div>
   );
