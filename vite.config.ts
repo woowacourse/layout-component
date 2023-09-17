e90a26ffd29d07c9da26eacdd4ac72aacb21d4ab
kyw0716 <kyw0716@naver.com>
2023-09-17 23:04:55 +0900

    chore: npm 배포를 위한 패키지 설치

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
+import dts from 'vite-plugin-dts';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/lib/index.tsx'),
+      name: 'index',
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        globals: {
+          react: 'React',
+        },
+      },
+    },
+    commonjsOptions: {
+      esmExternals: ['react'],
+    },
+  },
+  plugins: [react(), dts()],
+});
