b8d2d93ae4cf126725b1fdeb7162b439f4532e9d
jeongwusi <jeongwusi@gmail.com>
2023-09-17 19:28:03 +0900

    ♻️ refactor: 파일 구조 수정

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// Export Container 컴포넌트
+export { default as Container } from "./Container/Container";
+
+// Export Grid 컴포넌트
+export { default as Grid } from "./Grid/Grid";
+
+// Export Flex 컴포넌트
+export { default as Flex } from "./Flex/Flex";
