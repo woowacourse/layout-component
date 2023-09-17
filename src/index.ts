848427ab0eaa21013fbb4808ee4e161746223e25
jeongwusi <jeongwusi@gmail.com>
2023-09-17 20:28:06 +0900

    📦 chore: index.ts의 불필요한 주석 제거와 라이브러리 버전 수정

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
-// Export Container 컴포넌트
 export { default as Container } from "./Container/Container";
 
-// Export Grid 컴포넌트
 export { default as Grid } from "./Grid/Grid";
 
-// Export Flex 컴포넌트
 export { default as Flex } from "./Flex/Flex";
