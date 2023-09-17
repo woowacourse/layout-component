9fca2566a6289cc36cea30919fd091bbbc90d714
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-09-18 02:06:32 +0900

    refactor: Grid 스타일 분리

---
+++ src/layouts/Grid/Grid.styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import {GridProps} from "./Grid.tsx";
+
+export const StyledGrid = styled.div<GridProps>`
+  display: grid;
+  grid-template-rows: repeat(${({rows}) => rows}, 1fr);
+  grid-template-columns: repeat(${({columns}) => columns}, 1fr);
+  gap: ${({gap}) => gap}px;
+`;
--- src/layouts/Grid/Grid.tsx
+++ src/layouts/Grid/Grid.tsx
@@ -1,19 +1,12 @@
-import styled from "styled-components";
-import { ReactNode } from "react";
+import {ReactNode} from "react";
+import {StyledGrid} from "./Grid.styles.ts";
 
-interface GridProps {
+export interface GridProps {
   rows: number;
   columns: number;
   gap: number;
   children: ReactNode;
 }
-
-const StyledGrid = styled.div<GridProps>`
-  display: grid;
-  grid-template-rows: repeat(${({ rows }) => rows}, 1fr);
-  grid-template-columns: repeat(${({ columns }) => columns}, 1fr);
-  gap: ${({ gap }) => gap}px;
-`;
 
 const Grid = ({ rows, columns, gap, children }: GridProps) => {
   return (
