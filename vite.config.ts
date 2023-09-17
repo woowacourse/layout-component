265a5ca4941d1032574a8ab0eb0e112e6863e3eb
dahyeee <06robin11@gmail.com>
2023-09-17 22:31:45 +0900

    chore: npm배포를 위한 환경설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), dts()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'layout-component-dahyeeee',
+      fileName: (format) => `layout-component-dahyeeee.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        globals: {
+          react: 'React',
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@src': path.resolve(__dirname, 'src'),
+    },
+  },
+  optimizeDeps: {
+    exclude: ['@src/stories'],
+  },
+});
