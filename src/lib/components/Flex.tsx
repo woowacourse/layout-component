14d7095e28551ca3647ad8de9574a26bd3555234
yeopto <gunyeop530@gmail.com>
2023-09-18 00:27:15 +0900

    feat: Flex 컴포넌트 구현

---
+++ src/lib/components/Flex.tsx
@@ -0,0 +1,30 @@
+import { ReactNode } from 'react';
+import styled from 'styled-components';
+
+type Props = {
+  direction: 'row' | 'column';
+  justify: 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around';
+  align: 'flex-start' | 'flex-end' | 'center' | 'stretch';
+  gap?: number;
+  children: ReactNode;
+};
+
+const Flex = ({ direction, justify, align, gap, children }: Props) => {
+  return (
+    <Layout direction={direction} justify={justify} align={align} gap={gap}>
+      {children}
+    </Layout>
+  );
+};
+
+export default Flex;
+
+type LayoutProps = Omit<Props, 'children'>;
+
+const Layout = styled.div<LayoutProps>`
+  display: flex;
+  flex-direction: ${props => `${props.direction}`};
+  justify-content: ${props => `${props.justify}`};
+  align-items: ${props => `${props.align}`};
+  gap: ${props => `${props.gap ? `${props.gap}px` : 'none'}`};
+`;
