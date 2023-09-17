25b0a026132887833487f9b125c0cacd77481633
2yunseong <dbsdltjd3701@naver.com>
2023-09-17 19:39:32 +0900

    feat: grid item 속성 추가

--- src/components/GridItem/index.tsx
+++ src/components/GridItem/index.tsx
@@ -1,12 +1,27 @@
 import { PropsWithChildren } from 'react';
 import styled from 'styled-components';
 
-interface GridItemProps extends PropsWithChildren {}
+type GridBoundary = `${number}/${number}`;
+interface GridItemProps extends PropsWithChildren {
+  gridCol: GridBoundary | 'auto';
+  gridRow: GridBoundary | 'auto';
+}
 
-const GridItemComponent = styled.div``;
+const GridItemComponent = styled.div<GridItemProps>`
+  grid-column: ${({ gridCol }) => (!gridCol ? 'auto' : gridCol)};
+  grid-row: ${({ gridRow }) => (!gridRow ? 'auto' : gridRow)};
+`;
 
-const GridItem = ({ children }: GridItemProps) => {
-  return <GridItemComponent>{children}</GridItemComponent>;
+const GridItem = ({
+  gridRow = 'auto',
+  gridCol = 'auto',
+  children,
+}: Partial<GridItemProps>) => {
+  return (
+    <GridItemComponent gridRow={gridRow} gridCol={gridCol}>
+      {children}
+    </GridItemComponent>
+  );
 };
 
 export default GridItem;
