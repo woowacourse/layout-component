788f12d448a6a4d3a934a36bf861ded3a58f9d8e
Dohyeon Lee <dlehgus1110@naver.com>
2023-09-17 19:23:07 +0900

    feat: flex 컴포넌트 구현

--- src/lib/Flex/Flex.tsx
+++ src/lib/Flex/Flex.tsx
@@ -1,5 +1,39 @@
-const Flex = () => {
-  return <div>Flex</div>;
+import { PropsWithChildren } from 'react';
+
+type FlexProps<C extends React.ElementType> = {
+  as?: C;
+  direction?: React.CSSProperties['flexDirection'];
+  justify?: React.CSSProperties['justifyContent'];
+  align?: React.CSSProperties['alignItems'];
+  gap?: React.CSSProperties['gap'];
+  wrap?: React.CSSProperties['flexWrap'];
+} & React.ComponentPropsWithoutRef<C>;
+
+const Flex = <C extends React.ElementType = 'div'>({
+  as,
+  direction,
+  justify,
+  align,
+  gap,
+  wrap,
+  children,
+  ...atributtes
+}: PropsWithChildren<FlexProps<C>>) => {
+  const styles = {
+    display: 'flex',
+    justifyContent: justify,
+    alignItems: align,
+    gap,
+    flexWrap: wrap,
+  };
+
+  const Component = as || 'div';
+
+  return (
+    <Component style={styles} {...atributtes}>
+      {children}
+    </Component>
+  );
 };
 
 export default Flex;
