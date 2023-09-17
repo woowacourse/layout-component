b198b60ab2225756457d5568c89ad51cdace4965
Youngjin Park <youngjin988@gmail.com>
2023-09-17 22:03:14 +0900

    feat: `Container` 컴포넌트 추가

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,21 @@
+import { PropsWithChildren } from 'react';
+import styled, { CSSProperties, css } from 'styled-components';
+
+type Props = {
+  minWidth: CSSProperties['minWidth'];
+  maxWidth: CSSProperties['maxWidth'];
+};
+
+const Container = ({ children, ...rest }: PropsWithChildren<Props>) => {
+  return <StyledContainer {...rest}>{children}</StyledContainer>;
+};
+
+export default Container;
+
+const StyledContainer = styled.div<Props>`
+  ${({ minWidth, maxWidth }) => css`
+    min-width: ${minWidth};
+    max-width: ${maxWidth};
+  `};
+  margin: auto;
+`;
