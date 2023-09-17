5cbb00df86ae16921a1f8d020290baf61b08686a
Ashley Heo <ashleysyheo@gmail.com>
2023-09-17 16:01:12 +0900

    feat: Grid 컴포넌트 구현

---
+++ src/components/Grid.tsx
@@ -0,0 +1,115 @@
+/* eslint-disable react/display-name */
+import type { ComponentPropsWithoutRef, ElementType } from 'react';
+
+interface GridProps extends ComponentPropsWithoutRef<ElementType> {
+  /**
+   * Grid 컴포넌트가 사용할 HTML 태그
+   *
+   * @default 'div'
+   */
+  tag?: ElementType;
+  /**
+   * Grid 컴포넌트의 행의 크기를 지정하는 방법
+   */
+  rows?: string | number;
+  /**
+   * Grid 컴포넌트의 열의 크기를 지정하는 방법
+   */
+  columns?: string | number;
+  /**
+   * Grid 컴포넌트 내 아이템들 사이의 행 간격
+   *
+   * @default 'normal'
+   */
+  rowGap?: string | number;
+  /**
+   * Grid 컴포넌트 내 아이템들 사이의 열 간격
+   *
+   * @default 'normal'
+   */
+  columnGap?: string | number;
+  /**
+   * Grid 컴포넌트 내 아이템들 사이의 간격
+   *
+   * @default 'normal'
+   */
+  gap?: string | number;
+}
+
+function Grid({
+  tag = 'div',
+  rows,
+  columns,
+  rowGap,
+  columnGap,
+  gap,
+  children,
+  ...attributes
+}: GridProps) {
+  const Tag = tag;
+
+  console.log(rows, columns);
+  console.log(String(columns));
+
+  const gridTemplateRows = String(rows)?.match(/^\d+$/) ? `repeat(${rows}, minmax(0, 1fr))` : rows;
+  const gridTemplateColumns = String(columns)?.match(/^\d+$/)
+    ? `repeat(${columns}, minmax(0, 1fr))`
+    : columns;
+
+  console.log(gridTemplateColumns, gridTemplateRows);
+
+  return (
+    <Tag
+      css={{
+        display: 'grid',
+        gridTemplateRows,
+        gridTemplateColumns,
+        rowGap,
+        columnGap,
+        gap,
+      }}
+      {...attributes}
+    >
+      {children}
+    </Tag>
+  );
+}
+
+interface ItemProps extends ComponentPropsWithoutRef<ElementType> {
+  /**
+   * Item 컴포넌트가 사용할 HTML 태그
+   *
+   * @default 'div'
+   */
+  tag?: ElementType;
+  /**
+   * Grid에 대한 Item 컴포넌트의 위치 설정
+   *
+   * @default 'auto / auto'
+   */
+  area?: string;
+  /**
+   * Grid의 헹에 대한 Item 컴포넌트의 위치 설정
+   *
+   * @default 'auto / auto'
+   */
+  row?: string | number;
+  /**
+   * Grid의 열에 대한 Item 컴포넌트의 위치 설정
+   *
+   * @default 'auto / auto'
+   */
+  column?: string | number;
+}
+
+Grid.Item = ({ tag = 'div', area, row, column, children, ...attributes }: ItemProps) => {
+  const Tag = tag;
+
+  return (
+    <Tag css={{ gridArea: area, gridRow: row, gridColumn: column }} {...attributes}>
+      {children}
+    </Tag>
+  );
+};
+
+export default Grid;
--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,6 @@ import LayoutProvider from '@/LayoutProvider';
 
 import Container from '@components/Container';
 import Flex from '@components/Flex';
+import Grid from '@components/Grid';
 
-export { LayoutProvider, Container, Flex };
+export { LayoutProvider, Container, Flex, Grid };
