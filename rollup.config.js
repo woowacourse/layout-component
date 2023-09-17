bf561d49413f8da8609c69757675d38d17c6ae51
Leejin-Yang <ejin1996@gmail.com>
2023-09-17 15:40:00 +0900

    chore: postcss 플러그인 추가

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
 import { terser } from 'rollup-plugin-terser';
 
 const extensions = ['.js', '.jsx', '.ts', '.tsx'];
@@ -25,6 +26,12 @@ export default inputSrc.map(([input, format]) => ({
   plugins: [
     peerDepsExternal(),
     resolve({ extensions }),
+    postcss({
+      extract: true,
+      minimize: true,
+      modules: true,
+      plugins: [],
+    }),
     commonjs(),
     babel({
       babelHelpers: 'runtime',
