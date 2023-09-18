f4857944a0fc6b14165eda68a0cbfc6f5b06f477
ukkodeveloper <ukkodeveloper@gmail.com>
2023-09-18 09:33:58 +0900

    feat: theme으로 break points 설정

---
+++ src/style/theme.ts
@@ -0,0 +1,13 @@
+const theme = {
+  breakpoints: {
+    sm: '540px',
+    md: '720px',
+    lg: '960px',
+    xl: '1140px',
+    xxl: '1320px',
+  },
+
+  color: 'yellow',
+};
+
+export default theme;
