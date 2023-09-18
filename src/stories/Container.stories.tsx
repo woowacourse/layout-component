e182c31bca9bcbfa309ce31bf0d865b6d9f98465
hafnium1923 <hafnium1923@gmail.com>
2023-09-18 15:36:27 +0900

    test: Container 스토리북에 컴포넌트와 props 설명 추가

--- src/stories/Container.stories.tsx
+++ src/stories/Container.stories.tsx
@@ -4,7 +4,36 @@ import Container from "~/components/Container";
 const meta = {
   title: "Layout/Container",
   component: Container,
-  tags: ["autodocs"],
+  parameters: {
+    docs: {
+      description: {
+        component:
+          "화면의 폭에 따라 내용의 최대 폭을 제한하고 중앙에 배치하는 레이아웃 컴포넌트",
+      },
+    },
+  },
+  argTypes: {
+    maxWidth: { control: "text" },
+    minWidth: { control: "text" },
+    width: { control: "text" },
+    maxHeight: { control: "text" },
+    minHeight: { control: "text" },
+    height: { control: "text" },
+    margin: { control: "text" },
+    marginTop: { control: "text" },
+    marginBottom: { control: "text" },
+    marginLeft: { control: "text" },
+    marginRight: { control: "text" },
+    padding: { control: "text" },
+    paddingTop: { control: "text" },
+    paddingBottom: { control: "text" },
+    paddingLeft: { control: "text" },
+    paddingRight: { control: "text" },
+    style: {
+      control: { type: "object" },
+      description: "추가적인 스타일을 정의하는 방법",
+    },
+  },
 } satisfies Meta<typeof Container>;
 
 export default meta;
@@ -13,8 +42,22 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    maxWidth: 700,
-    height: 300,
+    maxWidth: "100%",
+    minWidth: 100,
+    width: 500,
+    maxHeight: "100%",
+    minHeight: 100,
+    height: 500,
+    margin: 0,
+    marginTop: 0,
+    marginBottom: 0,
+    marginLeft: 0,
+    marginRight: 0,
+    padding: 0,
+    paddingTop: 0,
+    paddingBottom: 0,
+    paddingLeft: 0,
+    paddingRight: 0,
     style: {
       border: "1px solid black",
     },
@@ -23,7 +66,22 @@ export const Default: Story = {
 
 export const DefaultWithChildren: Story = {
   args: {
-    maxWidth: 700,
+    maxWidth: "100%",
+    minWidth: 100,
+    width: 500,
+    maxHeight: "100%",
+    minHeight: 100,
+    height: 500,
+    margin: 0,
+    marginTop: 0,
+    marginBottom: 0,
+    marginLeft: 0,
+    marginRight: 0,
+    padding: 0,
+    paddingTop: 0,
+    paddingBottom: 0,
+    paddingLeft: 0,
+    paddingRight: 0,
     children: (
       <div style={{ display: "flex", justifyContent: "center" }}>
         안녕하세요
