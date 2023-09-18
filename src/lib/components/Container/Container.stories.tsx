519beb8c3e057c4b6c8af4a4a32883e99f0ffb46
chex <chex1004@gmail.com>
2023-09-18 14:46:57 +0900

    feat: backgroundColor 속성 타입 변경

--- src/lib/components/Container/Container.stories.tsx
+++ src/lib/components/Container/Container.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import Container from './Container';
-import { css } from 'styled-components';
+import { CSSProperties, css } from 'styled-components';
 
 const meta = {
   title: 'Components/Container',
@@ -36,12 +36,24 @@ export const Primary: Story = {
   render: (args) => {
     return (
       <>
-        <Container {...args} backgroundColor={'#bffff5'} />
-        <Container {...args} backgroundColor={'#98fff0'} />
-        <Container {...args} backgroundColor={'#64e3d0'} />
-        <Container {...args} backgroundColor={'#49b9a8'} />
-        <Container {...args} backgroundColor={'#2c9484'} />
+        <Container {...args} backgroundColor={hexToRgba(args.backgroundColor, 0.2) || '#bffff5'} />
+        <Container {...args} backgroundColor={hexToRgba(args.backgroundColor, 0.4) || '#98fff0'} />
+        <Container {...args} backgroundColor={hexToRgba(args.backgroundColor, 0.6) || '#64e3d0'} />
+        <Container {...args} backgroundColor={hexToRgba(args.backgroundColor, 0.8) || '#49b9a8'} />
+        <Container {...args} backgroundColor={args.backgroundColor || '#2c9484'} />
       </>
     );
   },
+};
+
+const hexToRgba = (hex?: CSSProperties['backgroundColor'], opacity: number = 1) => {
+  if (!hex) return;
+
+  // #을 제거하고 앞에 '0x'를 붙여 16진수를 10진수로 변환
+  const r = parseInt(String(hex).slice(1, 3), 16);
+  const g = parseInt(String(hex).slice(3, 5), 16);
+  const b = parseInt(String(hex).slice(5, 7), 16);
+
+  // RGBA 문자열로 반환
+  return `rgba(${r}, ${g}, ${b}, ${opacity})`;
 };
--- src/lib/components/Container/Container.tsx
+++ src/lib/components/Container/Container.tsx
@@ -1,5 +1,5 @@
 import { ComponentPropsWithRef, ElementType } from 'react';
-import { CSSProp, styled } from 'styled-components';
+import { CSSProp, CSSProperties, styled } from 'styled-components';
 
 interface Props extends ComponentPropsWithRef<ElementType> {
   /** 컴포넌트의 HTML 태그를 지정하는 속성입니다. 예를 들어, tag="div"로 설정하면 컴포넌트가 <div> 태그로 렌더링됩니다. */
@@ -13,7 +13,7 @@ interface Props extends ComponentPropsWithRef<ElementType> {
   /** 컴포넌트의 최대 높이를 지정하는 속성입니다. 단위를 포함하여 입력해주세요.*/
   maxHeight?: string;
   /** 컴포넌트의 배경색상을 지정하는 속성입니다. */
-  backgroundColor?: string;
+  backgroundColor?: CSSProperties['backgroundColor'];
   /** 컨테이너에 적용할 CSS 스타일을 설정하는 속성입니다. */
   css?: CSSProp;
 }
