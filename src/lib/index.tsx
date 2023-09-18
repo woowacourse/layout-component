9eef41746fd10791364af3143c0cb60ff5086c5d
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-09-18 16:06:06 +0900

    chore: 배포 설정 수정

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -1,3 +1,3 @@
-export {default as Container} from "./layouts/Container";
-export {default as Flex} from "./layouts/Flex";
-export {default as Grid} from "./layouts/Grid";
+export {default as Container} from "./layouts/Container/Container";
+export {default as Flex} from "./layouts/Flex/Flex";
+export {default as Grid} from "./layouts/Grid/Grid";
