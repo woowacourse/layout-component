295c2fdd5bc2a9ca43b596c7d4e9b187bdf99a66
Youngjin Park <youngjin988@gmail.com>
2023-09-18 15:46:20 +0900

    chore: `Grid` 스토리 추가

---
+++ src/components/Grid/Grid.stories.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Grid from './index';
+const Item = <div style={{ backgroundColor: '#0064ff', padding: '10px' }}>아이템</div>;
+
+const meta = {
+  title: 'Example/Grid',
+  component: Grid,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  args: {
+    rows: 3,
+    columns: 3,
+    gap: '10px',
+    children: Array.from({ length: 9 }).map(() => Item),
+  },
+  argTypes: {
+    rows: {},
+    columns: {},
+    gap: {},
+    children: {},
+  },
+} satisfies Meta<typeof Grid>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Playground: Story = {};
+
+export const Grid2X5: Story = {
+  args: {
+    rows: 2,
+    columns: 5,
+    children: Array.from({ length: 10 }).map(() => Item),
+  },
+};
+
+export const GridGap30px: Story = {
+  args: {
+    gap: ' 30px',
+  },
+};
