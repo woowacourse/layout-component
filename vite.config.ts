b641aea252b9fd100b9048bb6cb219de511492c6
Suyoung Jung <jsy19970512@naver.com>
2023-09-18 12:51:00 +0900

    fix: Next.js 페이지 배포 시 에러 수정을 위한 옵션 추가

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,19 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import path from 'path';
 import dts from 'vite-plugin-dts';
+import pkg from './package.json';
+
+const makeExternalPredicate = (externalArr: string[]) => {
+  if (externalArr.length === 0) {
+    return () => false;
+  }
+
+  const pattern = new RegExp(`^(${externalArr.join('|')})($|/)`);
+
+  return (id: string) => pattern.test(id);
+};
+
+const externals = makeExternalPredicate(Object.keys(pkg.peerDependencies));
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,10 +26,13 @@ export default defineConfig({
       fileName: 'index',
     },
     rollupOptions: {
-      external: ['react'],
+      external: externals,
       output: {
         globals: {
           react: 'React',
+          '@emotion/react/jsx-runtime': 'jsxRuntime',
+          '@emotion/react': 'react',
+          '@emotion/styled': 'styled',
         },
       },
     },
