0422a887acf0f6ad924ce2205beed05dccfaa810
nangkyeonglim <nangkyeonglim@gmail.com>
2023-09-18 14:09:12 +0900

    fix: export 경로 오류 수정

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { default as Container } from './components/Container';
-export { default as Flex } from './components/Container';
-export { default as Grid } from './components/Container';
+export { default as Flex } from './components/Flex';
+export { default as Grid } from './components/Grid';
