070c759d4cb0731f473c198d65108123f4e29be9
ukkodeveloper <ukkodeveloper@gmail.com>
2023-09-18 16:31:31 +0900

    chore: 배포 환경설정 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/lib/index.ts'),
+      name: 'ukko-layout-component',
+      formats: ['es'],
+      fileName: `index`,
+    },
+    rollupOptions: {
+      external: ['react', 'styled-components'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'styled-components': 'styled',
+        },
+      },
+    },
+  },
+  plugins: [react(), dts({ insertTypesEntry: true })],
+});
