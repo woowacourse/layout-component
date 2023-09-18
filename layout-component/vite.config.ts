32e651d267da80e376a33818f7d9561ff8ea606a
afds4567 <33995840+afds4567@users.noreply.github.com>
2023-09-18 12:46:07 +0900

    chore: 배포 관련 설정

--- layout-component/vite.config.ts
+++ layout-component/vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/lib/index.tsx"),
+      name: "layout-component",
+      fileName: "layout-component",
+    },
+    rollupOptions: {
+      external: ["react"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+    commonjsOptions: {
+      esmExternals: ["react"],
+    },
+  },
+  plugins: [dts()],
+});
