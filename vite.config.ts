90c9ac81a9c7ff9db05d55e1ed1c51f4ecb91a6e
Dain Lee <feb.dain@gmail.com>
2023-09-18 17:55:09 +0900

    fix: 배포 스크립트 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,8 @@
 import react from '@vitejs/plugin-react';
-import * as path from 'path';
 import { defineConfig } from 'vite';
-import dts from 'vite-plugin-dts';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/lib/index.tsx'),
-      name: 'index',
-      fileName: 'index',
-    },
-    rollupOptions: {
-      external: ['react'],
-      output: {
-        globals: {
-          react: 'React',
-        },
-      },
-    },
-    commonjsOptions: {
-      esmExternals: ['react'],
-    },
-  },
-  plugins: [react(), dts(), tsconfigPaths()],
-  base: '/layout-component/',
+  plugins: [react(), tsconfigPaths()],
 });
