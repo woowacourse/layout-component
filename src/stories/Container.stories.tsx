11e2ec79d28725f54341c4ab423d0ca29c55c050
guridaek <xylitol79@gmail.com>
2023-09-18 15:10:18 +0900

    test: Container 스토리 변경

--- src/stories/Container.stories.tsx
+++ src/stories/Container.stories.tsx
@@ -15,7 +15,7 @@ type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
   args: {
-    children: Array.from({ length: 9 }).map((_, index) => <button>{index + 1}</button>),
+    children: Array.from({ length: 3 }).map((_, index) => <button>{index + 1}</button>),
     minWidth: "100px",
     maxWidth: "200px",
   },
