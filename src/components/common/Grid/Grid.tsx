2f30ff5c2b585e71696b8a6c59baeb698823fafa
AhnGeunTaek <dksrmsxor@gmail.com>
2023-09-18 16:39:50 +0900

    refactor: Grid 컴포넌트에 CSS Prop 추가

--- src/components/common/Grid/Grid.tsx
+++ src/components/common/Grid/Grid.tsx
@@ -1,15 +1,17 @@
 import styled from "styled-components";
 import { GridLayoutProps, GridProps } from "./type";
 
-const Grid = ({ row, column, gap, children }: GridProps) => {
+const Grid = ({ row, column, gap, css, children }: GridProps) => {
   return (
-    <GridLayout $row={row} $column={column} $gap={gap}>
+    <GridLayout $row={row} $column={column} $gap={gap} $css={css}>
       {children}
     </GridLayout>
   );
 };
 
 const GridLayout = styled.div<GridLayoutProps>`
+  ${({ $css }) => $css}
+
   width: 100%;
   display: grid;
   grid-template-columns: repeat(${({ $column }) => $column}, 1fr);
