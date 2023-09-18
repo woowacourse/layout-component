126613ee6e4a0d153acf55fe4dec4ecfc7808f28
inyeong-kang <tomatorkd@naver.com>
2023-09-18 11:38:33 +0900

    chore: 파일명 변경

--- src/lib/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as Button } from './Button/Button';
-export { default as Container } from './Container/Container';
-export { default as Flex } from './Flex/Flex';
-export { default as Grid } from './Grid/Grid';
