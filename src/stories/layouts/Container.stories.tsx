ce8f84422fee27b09ddeaf217a385fbaffba6bb9
hozzijeong <ghwls0905@naver.com>
2023-09-18 13:53:47 +0900

    test: Container 스토리 작성

--- src/stories/layouts/Container.stories.tsx
+++ src/stories/layouts/Container.stories.tsx
@@ -1,11 +1,38 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import Container from '../../components/layouts/Container';
+import Box from '../../components/Box';
 
 const meta = {
 	title: 'Layout/Container',
 	component: Container,
 	parameters: {
 		layout: 'centered',
+	},
+	argTypes: {
+		maxWidth: {
+			control: { type: 'text' },
+			description: 'Container의 최대 길이',
+			defaultValue: '300px',
+		},
+		minWidth: {
+			control: { type: 'text' },
+			description: 'Container의 최소 길이',
+			defaultValue: '100px',
+		},
+		tag: {
+			control: { type: 'select' },
+			options: ['div', 'section', 'main', 'header'],
+			description: 'Container의 시맨틱 태그',
+			defaultValue: 'div',
+		},
+		customCss: {
+			control: { type: 'object' },
+			description: '위에 속성 외에 개인적으로 설정할 값들',
+			defaultValue: {
+				width: '512px',
+				height: '512px',
+			},
+		},
 	},
 	tags: ['autodocs'],
 } satisfies Meta<typeof Container>;
@@ -18,12 +45,14 @@ export const Default: Story = {
 		maxWidth: '300px',
 		minWidth: '100px',
 		tag: 'section',
-		customCss: {
-			background: 'red',
-		},
 	},
-
 	render: ({ ...args }) => {
-		return <Container {...args}>Cool~</Container>;
+		return (
+			<Container {...args}>
+				{Array.from({ length: 10 }).map((_, idx) => (
+					<Box>{idx + 1}번 박스</Box>
+				))}
+			</Container>
+		);
 	},
 };
