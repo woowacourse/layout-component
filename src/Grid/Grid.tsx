744784ea0f1b6fa6ec8f661325f3ece32c8152ca
SangwonKang <72087183+ksone02@users.noreply.github.com>
2023-09-18 17:25:08 +0900

    feat: Grid 컴포넌트 구현

---
+++ src/Grid/Grid.tsx
@@ -0,0 +1,68 @@
+import { ComponentPropsWithoutRef } from 'react';
+import styled from 'styled-components';
+import { convertToPixel, generateDirectionStyle } from '../utils';
+import { ElementBasicStyleType } from '../types/styles';
+import { Combine, SCProps } from '../types/utility';
+
+type GridStyleProps = {
+  row?: number;
+  column?: number;
+  gap?: {
+    row?: number;
+    column?: number;
+  };
+};
+
+type GridContainerProps = ElementBasicStyleType & GridStyleProps;
+
+type GridProps = Combine<GridContainerProps, ComponentPropsWithoutRef<'div'>>;
+
+const Grid = (props: GridProps) => {
+  const {
+    children,
+    minWidth,
+    maxWidth,
+    margin,
+    padding,
+    row,
+    column,
+    gap,
+    ...restProps
+  } = props;
+
+  return (
+    <GridWrapper
+      $minWidth={minWidth}
+      $maxWidth={maxWidth}
+      $row={row}
+      $column={column}
+      $gap={gap}
+      $margin={margin}
+      $padding={padding}
+      {...restProps}
+    >
+      {children}
+    </GridWrapper>
+  );
+};
+
+export default Grid;
+
+type GirdSCProps = SCProps<GridContainerProps>;
+
+const GridWrapper = styled.div<GirdSCProps>`
+  display: grid;
+
+  min-width: ${({ $minWidth }) => convertToPixel($minWidth) ?? 0};
+  max-width: ${({ $maxWidth }) => convertToPixel($maxWidth) ?? 'unset'};
+
+  ${({ $padding }) => generateDirectionStyle('padding', $padding)}
+  ${({ $margin }) => generateDirectionStyle('margin', $margin)};
+
+  grid-template-rows: ${({ $row }) => $row ?? 'initial'};
+  grid-template-columns: ${({ $column }) => $column ?? 'initial'};
+  grid-row-gap: ${({ $gap }) =>
+    ($gap && convertToPixel($gap.row)) ?? 'initial'};
+  grid-column-gap: ${({ $gap }) =>
+    ($gap && convertToPixel($gap.column)) ?? 'initial'};
+`;
