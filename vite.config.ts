20942eb560868c415f26720df63841ba9bd6b31b
AhnGeunTaek <dksrmsxor@gmail.com>
2023-09-18 13:43:28 +0900

    chore: vite 설치 및 초기 셋팅

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/main.ts"),
+      formats: ["es"],
+    },
+  },
+});
