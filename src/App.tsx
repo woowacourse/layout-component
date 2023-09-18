d72c81e66fa360937d020a8d2630b070164174a6
AhnGeunTaek <dksrmsxor@gmail.com>
2023-09-18 16:13:54 +0900

    feat: Container 컴포넌트 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import Container from "./components/common/Container";
+
 function App() {
-  return <></>;
+  return (
+    <Container
+      maxWidth={1250}
+      minWidth={600}
+      css={{ backgroundColor: "#FAF1E4" }}
+    >
+      Container Component
+    </Container>
+  );
 }
 
 export default App;
