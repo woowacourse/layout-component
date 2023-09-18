43b0f591610387533e591306be783020ff80ef50
Gilpop8663 <wolfye@naver.com>
2023-09-18 09:47:37 +0900

    feat: commonJS와 ModuleJS를 지원하기 위해 명령어 추가 및 tsconfig 설정 변경 배포에 필요한 sh 명령어 파일 추가 필요없는 파일 삭제

--- src/lib/Container/index.js
+++
@@ -1,9 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-export default function Container({ ref, css, minWidth = 0, maxWidth = 0, children, }) {
-    return (_jsx("div", { ref: ref, style: {
-            width: '100vw',
-            minWidth: minWidth ? `${minWidth}px` : '',
-            maxWidth: maxWidth ? `${maxWidth}px` : '',
-            ...css,
-        }, children: children }));
-}
