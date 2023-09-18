d6cade0aa64db341c6fa2f6d40d5256cbb6b1295
n0eyes <doll4586633@naver.com>
2023-09-18 14:41:13 +0900

    refactor: storybook docs를 위해 타입을 한 파일에 작성
    - Container.type.ts 제거 > Container에 props type 선언
    - props 별 설명 추가

--- src/Container/Container.tsx
+++ src/Container/Container.tsx
@@ -1,11 +1,16 @@
-import {
-  CSSProperties,
-  ElementType,
-  PropsWithChildren,
-  ReactHTML,
-} from 'react';
-import { ContainerProps } from './Container.type';
+import { CSSProperties, HTMLProps, PropsWithChildren, ReactHTML } from 'react';
 import Flex from '../Flex/Flex';
+
+interface ContainerProps<T extends keyof ReactHTML> extends HTMLProps<T> {
+  /** 태그(tag) 프로퍼티는 ReactHTML의 키(key) 중 하나를 가질 수 있습니다. */
+  tag?: T;
+  /** maxWidth 프로퍼티는 Flex 컴포넌트 내용물의 최대 너비를 제한합니다. */
+  maxWidth?: CSSProperties['maxWidth'];
+  /** minWidth 프로퍼티는 Flex 컴포넌트 내용물의 최소 너비를 지정합니다. */
+  minWidth?: CSSProperties['minWidth'];
+  /** style 프로퍼티는 Flex 컴포넌트에 적용할 CSS 스타일을 지정합니다. */
+  style?: CSSProperties;
+}
 
 const Container = <T extends keyof ReactHTML>(
   props: PropsWithChildren<ContainerProps<T>>
