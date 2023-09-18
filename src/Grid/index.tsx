2e59d0f1ab3f2b852ff64004c15896c7bac16924
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-09-18 16:32:46 +0900

    feat: Grid 컴포넌트 구현

---
+++ src/Grid/index.tsx
@@ -0,0 +1,42 @@
+import { ComponentPropsWithoutRef, ReactNode } from 'react';
+import styled, { css } from 'styled-components';
+
+interface GridProps extends ComponentPropsWithoutRef<'div'> {
+  rows?: number;
+  columns?: number;
+  gap?: { row: number; column: number };
+  children?: ReactNode;
+}
+
+const Grid = ({ rows, columns, gap, children }: GridProps) => {
+  return (
+    <Wrapper rows={rows} columns={columns} gap={gap}>
+      {children}
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div<GridProps>`
+  display: grid;
+
+  ${({ columns }) =>
+    columns &&
+    css`
+      grid-template-columns: ${'1fr '.repeat(columns).trim()};
+    `}
+
+  ${({ rows }) =>
+    rows &&
+    css`
+      grid-template-rows: ${'1fr '.repeat(rows).trim()};
+    `}
+
+    ${({ gap }) =>
+    gap &&
+    css`
+      grid-column-gap: ${gap.column};
+      grid-row-gap: ${gap.row};
+    `}
+`;
+
+export default Grid;
