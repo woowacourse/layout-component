a3ea011f6a39e0d6970fe9a80a6ab8c7735a51fe
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-09-18 15:24:32 +0900

    chore: 배포를 위한 빌드 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,27 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import * as path from "path";
 
-// https://vitejs.dev/config/
+import {defineConfig} from "vite";
+import dts from "vite-plugin-dts";
+
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "lib/index.ts"),
+      name: "index",
+      fileName: "index",
+    },
+    rollupOptions: {
+      external: ["react"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+    commonjsOptions: {
+      esmExternals: ["react"],
+    },
+  },
+  plugins: [dts()],
   base: "/layout-component/",
 });
