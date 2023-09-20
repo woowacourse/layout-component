db62e80f5723d8d2653cfb7d36683dba3d3c7aa1
hozzijeong <ghwls0905@naver.com>
2023-09-20 11:28:15 +0900

    design: box 컴포넌트 디자인 수정

--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -1,12 +1,19 @@
 import { PropsWithChildren } from 'react';
 
 const Box = ({ children }: PropsWithChildren) => {
+	const background = '#' + Math.round(Math.random() * 0xffffff).toString(16);
 	return (
 		<div
 			style={{
-				width: 100,
-				height: 100,
-				background: 'red',
+				width: 200,
+				height: 200,
+				opacity: 0.7,
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+				color: 'white',
+				font: '600 1.6rem/2rem',
+				background,
 			}}
 		>
 			{children}
