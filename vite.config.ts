849ab979986fb54fee4d82b4ab7676cb18a99fe3
Dain Lee <feb.dain@gmail.com>
2023-09-20 17:42:25 +0900

    revert: vite config 되돌리기

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import dts from 'vite-plugin-dts';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts(), tsconfigPaths()],
-  base: '/layout-component/',
+  plugins: [react(), tsconfigPaths()],
 });
