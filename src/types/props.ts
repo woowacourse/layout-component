feef970f7917a047b850b8ba168a203bc8f60256
n0eyes <doll4586633@naver.com>
2023-09-20 15:23:12 +0900

    refactor: 공통 레이아웃 props type 분리

---
+++ src/types/props.ts
@@ -0,0 +1,19 @@
+import {
+  ComponentPropsWithoutRef,
+  ElementType,
+  PropsWithChildren,
+} from 'react';
+
+type PropsWithTag<T extends ElementType, P> = P & {
+  /** 태그(tag) 프로퍼티는 ReactHTML의 키(key) 중 하나를 가질 수 있습니다. */
+  tag?: T;
+};
+
+type PropsToOmit<T extends ElementType, P = {}> = keyof (T & P);
+
+type Props<T extends ElementType, P = {}> = PropsWithChildren<
+  PropsWithTag<T, P>
+> &
+  Omit<ComponentPropsWithoutRef<T>, PropsToOmit<T, P>>;
+
+export type { Props };
