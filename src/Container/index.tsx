b36c6ea0257d3f02e9da7c528b1c32383aa9cc6f
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-09-21 23:05:29 +0900

    feat: Container redesign
    minWidth, maxWidth 입력 값 제한

--- src/Container/index.tsx
+++ src/Container/index.tsx
@@ -1,9 +1,12 @@
 import { ComponentPropsWithoutRef, ReactNode } from 'react';
 import styled from 'styled-components';
+import convertToPercentage from '../utils/convertToPercentage';
+
+import { Slash } from '../@types';
 
 interface ContainerProps extends ComponentPropsWithoutRef<'div'> {
-  minWidth: string;
-  maxWidth: string;
+  minWidth: 'auto' | 'full' | 'screen' | Slash;
+  maxWidth: 'auto' | 'full' | 'screen' | Slash;
   children?: ReactNode;
 }
 
@@ -16,8 +19,18 @@ const Container = ({ minWidth, maxWidth, children }: ContainerProps) => {
 };
 
 const Wrapper = styled.div<ContainerProps>`
-  min-width: ${({ minWidth }) => minWidth};
-  max-width: ${({ maxWidth }) => maxWidth};
+  min-width: ${({ minWidth }) => {
+    if (minWidth === 'auto') return minWidth;
+    if (minWidth === 'full') return '100%';
+    if (minWidth === 'screen') return '100vw';
+    return convertToPercentage(minWidth);
+  }};
+  max-width: ${({ maxWidth }) => {
+    if (maxWidth === 'auto') return maxWidth;
+    if (maxWidth === 'full') return '100%';
+    if (maxWidth === 'screen') return '100vw';
+    return convertToPercentage(maxWidth);
+  }};
 `;
 
 export default Container;
