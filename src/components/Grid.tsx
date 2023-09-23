e3de8b11275bcc8517bc0db7cff76c1b56c91d34
cruelladevil <dev.timetravel@gmail.com>
2023-09-23 16:16:58 +0900

    feat: Grid 컴포넌트 수정
    - 다형성 컴포넌트 타입 적용
    - style과 attribute 모두 수용하도록 수정
    - CSSProperties를 기반으로 interface 수정

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,59 +1,74 @@
-import { ComponentPropsWithoutRef, ElementType } from 'react';
+import { CSSProperties, forwardRef } from 'react';
+import { PolymorphicComponentPropWithRef, PolymorphicRef } from '../types/polymorphic';
 
-interface GridProps extends ComponentPropsWithoutRef<ElementType> {
-  /**
-   * Grid 컴포넌트의 태그를 변경합니다.
-   * @default div
-   */
-  as?: ElementType;
-  /**
-   * `grid-template-rows` 스타일 프로퍼티를 정의합니다.
-   */
-  rows?: number;
-  /**
-   * `grid-template-columns` 스타일 프로퍼티를 정의합니다.
-   */
-  columns?: number;
-  /**
-   * `grid-row-gap` 스타일 프로퍼티를 정의합니다.
-   * @default 0
-   */
-  rowGap?: number;
-  /**
-   * `grid-column-gap` 스타일 프로퍼티를 정의합니다.
-   * @default 0
-   */
-  columnGap?: number;
-  /**
-   * `gap` 스타일 프로퍼티를 정의합니다.
-   * @default 0
-   */
-  gap?: number;
-}
+type GridStyleProps = Omit<CSSProperties, 'gridTemplateRows' | 'gridTemplateColumns' | 'rowGap' | 'columnGap' | 'gap'>;
 
-const Grid = ({ as = 'div', rows, columns, rowGap, columnGap, gap, children, ...attributes }: GridProps) => {
-  const Component = as;
+type GridProps<C extends React.ElementType> = PolymorphicComponentPropWithRef<
+  C,
+  {
+    /**
+     * `grid-template-rows` 스타일 프로퍼티를 정의합니다.
+     */
+    rows?: number;
+    /**
+     * `grid-template-columns` 스타일 프로퍼티를 정의합니다.
+     */
+    columns?: number;
+    /**
+     * `grid-row-gap` 스타일 프로퍼티를 정의합니다.
+     */
+    rowGap?: CSSProperties['rowGap'];
+    /**
+     * `grid-column-gap` 스타일 프로퍼티를 정의합니다.
+     */
+    columnGap?: CSSProperties['columnGap'];
+    /**
+     * `gap` 스타일 프로퍼티를 정의합니다.
+     */
+    gap?: CSSProperties['gap'];
+    /**
+     * 추가적인 스타일링을 정의합니다.
+     */
+    style?: GridStyleProps;
+  }
+>;
 
-  const gridTemplateRows = String(rows)?.match(/^\d+$/) ? `repeat(${rows}, minmax(0, 1fr))` : rows;
-  const gridTemplateColumns = String(columns)?.match(/^\d+$/) ? `repeat(${columns}, minmax(0, 1fr))` : columns;
+type GridComponent = <C extends React.ElementType>(props: GridProps<C>) => React.ReactNode;
 
-  return (
-    <Component
-      css={[
-        {
-          display: 'grid',
-          gridTemplateRows,
-          gridTemplateColumns,
-          rowGap,
-          columnGap,
-          gap,
-        },
-        { ...attributes },
-      ]}
-    >
-      {children}
-    </Component>
-  );
-};
+/**
+ * Docs:
+ * - [Grid Docs](https://6507fe70aa120a5453701630-uaqgayiynj.chromatic.com/?path=/docs/example-grid--docs)
+ */
+const Grid: GridComponent = forwardRef(
+  <C extends React.ElementType>(
+    { rows, columns, rowGap, columnGap, gap, as, style, children, ...attributes }: GridProps<C>,
+    ref?: PolymorphicRef<C>,
+  ) => {
+    const Component = as || 'div';
+
+    const gridTemplateRows = String(rows)?.match(/^\d+$/) ? `repeat(${rows}, minmax(0, 1fr))` : rows;
+    const gridTemplateColumns = String(columns)?.match(/^\d+$/) ? `repeat(${columns}, minmax(0, 1fr))` : columns;
+
+    return (
+      <Component
+        css={[
+          {
+            display: 'grid',
+            gridTemplateRows,
+            gridTemplateColumns,
+            rowGap,
+            columnGap,
+            gap,
+          },
+          { ...style },
+        ]}
+        ref={ref}
+        {...attributes}
+      >
+        {children}
+      </Component>
+    );
+  },
+);
 
 export default Grid;
