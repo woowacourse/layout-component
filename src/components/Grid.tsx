58be2d333869375e8ba1c708d594d7a297a66d99
nangkyeonglim <nangkyeonglim@gmail.com>
2023-09-24 06:59:47 +0900

    feat:`Grid` 컴포넌트에 반응형 추가

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,8 +1,25 @@
-import { ComponentPropsWithoutRef } from 'react';
+/** @jsxImportSource @emotion/react */
+
+import { CSSProperties, ComponentPropsWithoutRef } from 'react';
+import { css } from '@emotion/react';
+
+const GRID_BREAK_POINTS = {
+  xs: '0px',
+  sm: '600px',
+  md: '900px',
+  lg: '1200px',
+  xl: '1536px',
+};
+
+type GridBreakPoints = keyof typeof GRID_BREAK_POINTS;
+
+type ResponsiveColumns = Partial<{
+  [key in GridBreakPoints]: number;
+}>;
 
 interface Props extends ComponentPropsWithoutRef<'div'> {
   rows?: number;
-  columns?: number;
+  columns?: number | ResponsiveColumns;
   rowTrackList?: string;
   columnTrackList?: string;
   rowGap?: string;
@@ -19,18 +36,53 @@ const Grid = ({
   children,
   ...attributes
 }: Props) => {
+  const responsiveColumnStyle = (columns: Props['columns']) => {
+    const responsiveStyle: Record<string, CSSProperties> = {};
+
+    if (typeof columns === 'object') {
+      const sizes = Object.keys(columns) as GridBreakPoints[];
+
+      sizes.forEach((size, index) => {
+        const mediaQuery = `@media screen and (min-width: ${GRID_BREAK_POINTS[size]})`;
+        responsiveStyle[mediaQuery] = {
+          gridTemplateColumns: `repeat(${columns[size]}, ${columnTrackList})`,
+        };
+
+        if (index === sizes.length - 1) {
+          responsiveStyle['default'] = {
+            gridTemplateColumns: `repeat(${columns[size]}, ${columnTrackList})`,
+          };
+        }
+      });
+    }
+
+    if (typeof columns === 'number') {
+      responsiveStyle['default'] = {
+        gridTemplateColumns: `repeat(${
+          columns ?? 'auto-fill'
+        }, ${columnTrackList})`,
+      };
+    }
+
+    return responsiveStyle;
+  };
+
   const gridContainerStyle = {
     display: 'grid',
     gridTemplateRows: `repeat(${rows ?? 'auto-fill'}, ${rowTrackList})`,
-    gridTemplateColumns: `repeat(${
-      columns ?? 'auto-fill'
-    }, ${columnTrackList})`,
     columnGap,
     rowGap,
   };
 
   return (
-    <div style={gridContainerStyle} {...attributes}>
+    <div
+      css={css({
+        ...gridContainerStyle,
+        ...responsiveColumnStyle(columns)['default'],
+        ...responsiveColumnStyle(columns),
+      })}
+      {...attributes}
+    >
       {children}
     </div>
   );
