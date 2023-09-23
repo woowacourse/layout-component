79e5777289ac8bcf44cda79576ad56313a2820db
Youngjin Park <youngjin988@gmail.com>
2023-09-24 06:15:07 +0900

    chore: `jsdoc` 이용해 `Flex` prop 설명 추가

--- src/components/Flex/index.tsx
+++ src/components/Flex/index.tsx
@@ -2,9 +2,35 @@ import { PropsWithChildren } from 'react';
 import styled, { CSSProperties, css } from 'styled-components';
 
 type Props = {
+  /**
+   * Flex 컴포넌트의 HTML 태그
+   *
+   * @default 'div'
+   */
+  tag?: Tag;
+  /**
+   * Flex 컴포넌트의 flex-direction 속성
+   *
+   * @default 'row'
+   */
   direction?: CSSProperties['flexDirection'];
+  /**
+   * Flex 컴포넌트의 justify-content 속성
+   *
+   * @default 'center'
+   */
   justify?: CSSProperties['justifyContent'];
+  /**
+   * Flex 컴포넌트의 align-items 속성
+   *
+   * @default 'center'
+   */
   align?: CSSProperties['alignItems'];
+  /**
+   * Flex 컴포넌트의 gap 속성
+   *
+   * @default 'normal'
+   */
   gap?: CSSProperties['gap'];
   style?: CSSProperties;
 };
