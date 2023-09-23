8aa82663fb1f37d2b476e08a58d27114142ce2a7
gilpop8663 <wolfye@naver.com>
2023-09-24 02:08:45 +0900

    feat: 컴포넌트에서 사용할 공통 스타일 타입 선언 및 스타일드 컴포넌트 구현

--- src/lib/types/common.ts
+++ src/lib/types/common.ts
@@ -7,8 +7,37 @@ import {
 } from 'react';
 
 interface Common<T extends ElementType> extends PropsWithChildren {
-  as?: T;
+  as?: T | ElementType;
   css?: CSSProperties;
+
+  m?: string;
+  mx?: string;
+  my?: string;
+  ml?: string;
+  mr?: string;
+  mb?: string;
+  mt?: string;
+
+  mxAuto?: boolean;
+  myAuto?: boolean;
+
+  p?: string;
+  px?: string;
+  py?: string;
+  pl?: string;
+  pr?: string;
+  pb?: string;
+  pt?: string;
+
+  fontSize?: string;
+  fontWeight?: CSSProperties['fontWeight'];
+  textAlign?: CSSProperties['textAlign'] | string;
+
+  border?: string;
+  borderRadius?: string;
+
+  color?: string;
+  bgColor?: string;
 }
 
 export type CommonProps<T extends ElementType> = Common<T> &
