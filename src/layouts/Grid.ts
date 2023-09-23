bb14405f7954a570d1edbf1d9e9326c34216a4c5
semnil5202 <semnil5202@gmail.com>
2023-09-23 14:56:21 +0900

    refactor: 그리드 컴포넌트 CSSProperties 일괄 지정

--- src/layouts/Grid.ts
+++ src/layouts/Grid.ts
@@ -1,40 +1,17 @@
-import styled from 'styled-components';
+import { CSSProperties, styled } from 'styled-components';
 import Container, { Props as ContainerProps } from './Container';
 
-type JustifyItems = 'start' | 'end' | 'center' | 'stretch';
-type JustifyContent =
-  | 'start'
-  | 'end'
-  | 'center'
-  | 'stretch'
-  | 'space-around'
-  | 'space-between'
-  | 'space-evenly';
-type AlignItems = 'start' | 'end' | 'center' | 'stretch';
-type AlignContent =
-  | 'start'
-  | 'end'
-  | 'center'
-  | 'stretch'
-  | 'space-around'
-  | 'space-between'
-  | 'space-evenly';
-type JustifySelf = 'start' | 'end' | 'center' | 'stretch';
-type AlignSelf = 'start' | 'end' | 'center' | 'stretch';
-
-interface Props extends ContainerProps {
-  row: number | string;
-  column: number | string;
-  $justifyItems?: JustifyItems;
-  $justifyContent?: JustifyContent;
-  $alignItems?: AlignItems;
-  $alignContent?: AlignContent;
-  $justifySelf?: JustifySelf;
-  $alignSelf?: AlignSelf;
-  $gridTemplateAreas?: string;
-  $gridTemplateColumns?: string;
-  $gridTemplateRows?: string;
-  gap?: string;
+interface Props extends Omit<ContainerProps, 'display'> {
+  row: CSSProperties['gridRow'];
+  column: CSSProperties['gridColumn'];
+  $alignItems?: CSSProperties['alignItems'];
+  $alignContent?: CSSProperties['alignContent'];
+  $justifyContent?: CSSProperties['justifyContent'];
+  $justifyItems?: CSSProperties['justifyItems'];
+  $gridTemplateAreas?: CSSProperties['gridTemplateAreas'];
+  $gridTemplateColumns?: CSSProperties['gridTemplateColumns'];
+  $gridTemplateRows?: CSSProperties['gridTemplateRows'];
+  gap?: CSSProperties['gap'];
 }
 
 const Grid = styled(Container)<Props>`
@@ -45,8 +22,6 @@ const Grid = styled(Container)<Props>`
   justify-content: ${({ $justifyContent }) => $justifyContent};
   align-items: ${({ $alignItems }) => $alignItems};
   align-content: ${({ $alignContent }) => $alignContent};
-  justify-self: ${({ $justifySelf }) => $justifySelf};
-  align-self: ${({ $alignSelf }) => $alignSelf};
   grid-template-areas: ${({ $gridTemplateAreas }) => $gridTemplateAreas};
   grid-template-columns: ${({ $gridTemplateColumns }) => $gridTemplateColumns};
   grid-template-rows: ${({ $gridTemplateRows }) => $gridTemplateRows};
