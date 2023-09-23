e0b3a5d5eac28a038d4ab3689a8962c3525ad725
cruelladevil <dev.timetravel@gmail.com>
2023-09-23 16:17:09 +0900

    feat: Flex 컴포넌트 수정
    - 다형성 컴포넌트 타입 적용
    - style과 attribute 모두 수용하도록 수정
    - CSSProperties를 기반으로 interface 수정

--- src/components/Flex.tsx
+++ src/components/Flex.tsx
@@ -1,62 +1,84 @@
-import { ComponentPropsWithoutRef, ElementType } from 'react';
+import { CSSProperties, forwardRef } from 'react';
+import { PolymorphicComponentPropWithRef, PolymorphicRef } from '../types/polymorphic';
 
-interface FlexProps extends ComponentPropsWithoutRef<ElementType> {
-  /**
-   * `Flex` 컴포넌트의 태그를 변경합니다.
-   * @default div
-   */
-  as?: ElementType;
-  /**
-   * `flex-direction` 스타일 프로퍼티를 정의합니다.
-   * @default row
-   */
-  direction?: string;
-  /**
-   * `flex-wrap` 스타일 프로퍼티를 정의합니다.
-   * @default nowrap
-   */
-  wrap?: string;
-  /**
-   * `justify-content` 스타일 프로퍼티를 정의합니다.
-   * @default flex-start
-   */
-  justify?: string;
-  /**
-   * `align-items` 스타일 프로퍼티를 정의합니다.
-   * @default stretch
-   */
-  align?: string;
-  /**
-   * `gap` 스타일 프로퍼티를 정의합니다.
-   * @default 0
-   */
-  gap?: number;
-}
+type FlexStyleProps = Omit<CSSProperties, 'flexDirection' | 'flexWrap' | 'justifyContent' | 'alignItems' | 'gap'>;
 
-const Flex = ({
-  as = 'div',
-  direction = 'row',
-  wrap = 'nowrap',
-  justify = 'flex-start',
-  align = 'stretch',
-  gap,
-  children,
-}: FlexProps) => {
-  const Component = as;
+type FlexProps<C extends React.ElementType> = PolymorphicComponentPropWithRef<
+  C,
+  {
+    /**
+     * `flex-direction` 스타일 프로퍼티를 정의합니다.
+     * @default 'row'
+     */
+    direction?: CSSProperties['flexDirection'];
+    /**
+     * `flex-wrap` 스타일 프로퍼티를 정의합니다.
+     * @default 'nowrap'
+     */
+    wrap?: CSSProperties['flexWrap'];
+    /**
+     * `justify-content` 스타일 프로퍼티를 정의합니다.
+     * @default 'flex-start'
+     */
+    justify?: CSSProperties['justifyContent'];
+    /**
+     * `align-items` 스타일 프로퍼티를 정의합니다.
+     * @default 'stretch'
+     */
+    align?: CSSProperties['alignItems'];
+    /**
+     * `gap` 스타일 프로퍼티를 정의합니다.
+     */
+    gap?: CSSProperties['gap'];
+    /**
+     * 추가적인 스타일링을 정의합니다.
+     */
+    style?: FlexStyleProps;
+  }
+>;
 
-  return (
-    <Component
-      css={{
-        display: 'flex',
-        flexFlow: `${direction} ${wrap}`,
-        justifyContent: justify,
-        alignItems: align,
-        gap,
-      }}
-    >
-      {children}
-    </Component>
-  );
-};
+type FlexComponent = <C extends React.ElementType>(props: FlexProps<C>) => React.ReactNode;
+
+/**
+ * Docs:
+ * - [Flex Docs](https://6507fe70aa120a5453701630-uaqgayiynj.chromatic.com/?path=/docs/example-flex--docs)
+ */
+const Flex: FlexComponent = forwardRef(
+  <C extends React.ElementType>(
+    {
+      direction = 'row',
+      wrap = 'nowrap',
+      justify = 'flex-start',
+      align = 'stretch',
+      gap,
+      as,
+      style,
+      children,
+      ...attributes
+    }: FlexProps<C>,
+    ref?: PolymorphicRef<C>,
+  ) => {
+    const Component = as || 'div';
+
+    return (
+      <Component
+        css={[
+          {
+            display: 'flex',
+            flexFlow: `${direction} ${wrap}`,
+            justifyContent: justify,
+            alignItems: align,
+            gap,
+          },
+          { ...style },
+        ]}
+        ref={ref}
+        {...attributes}
+      >
+        {children}
+      </Component>
+    );
+  },
+);
 
 export default Flex;
