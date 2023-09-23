6fcc12c151659ce2a4c9d045ce5234483a5ec8dc
hae-on <solbi2004@naver.com>
2023-09-24 01:05:32 +0900

    refactor: tab list style 추가

--- src/TabList/TabList.tsx
+++ src/TabList/TabList.tsx
@@ -1,29 +1,71 @@
 import { ComponentPropsWithoutRef } from 'react';
 
+const defaultActiveStyle = {
+  color: '#6B6B6B',
+  borderColor: '#6B6B6B',
+  fontWeight: 'bold',
+};
+
+const defaultInactiveStyle = {
+  color: '#A0A0A0',
+  borderColor: '#DDDDDD',
+  fontWeight: 'normal',
+};
+
 interface TabListProps extends ComponentPropsWithoutRef<'li'> {
+  activeStyle?: {
+    color?: string;
+    backGroundColor?: string;
+    borderColor?: string;
+    fontWeight?: string;
+  };
+  inactiveStyle?: {
+    color?: string;
+    backGroundColor?: string;
+    borderColor?: string;
+    fontWeight?: string;
+  };
+  listWidth?: string;
+  padding?: string;
+  fontSize?: string;
   label: string;
   isActive: boolean;
   onClick: () => void;
 }
 
-const TabList = ({ label, isActive, onClick, ...props }: TabListProps) => {
+const TabList = ({
+  activeStyle = defaultActiveStyle,
+  inactiveStyle = defaultInactiveStyle,
+  listWidth = '100%',
+  padding = '20px',
+  fontSize = '16px',
+  label,
+  isActive,
+  onClick,
+  ...props
+}: TabListProps) => {
   return (
     <li
       style={{
+        width: listWidth,
         borderBottom: '2px solid',
-        borderColor: isActive ? '#6B6B6B' : '#DDDDDD',
+        borderColor: isActive
+          ? activeStyle.borderColor
+          : inactiveStyle.borderColor,
       }}
       {...props}
     >
       <button
         type='button'
         style={{
-          padding: '10px 20px',
-          border: 'none',
-          backgroundColor: 'transparent',
-          cursor: 'pointer',
-          color: isActive ? 'blue' : 'black',
-          fontWeight: isActive ? 'bold' : 'normal',
+          width: '100%',
+          padding,
+          fontSize,
+          color: isActive ? activeStyle.color : inactiveStyle.color,
+          fontWeight: isActive
+            ? activeStyle.fontWeight
+            : inactiveStyle.fontWeight,
+          textAlign: 'center',
         }}
         onClick={onClick}
       >
