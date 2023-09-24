caa74b6d0dfdb9467e54fa48d135f148dff9598a
cruelladevil <dev.timetravel@gmail.com>
2023-09-24 18:49:26 +0900

    remove: 사용되지 않는 webpack과 babel 설정 파일 삭제

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: [
-    ['@babel/preset-env'],
-    ['@babel/preset-react', { runtime: 'automatic', importSource: '@emotion/react' }],
-    ['@babel/preset-typescript'],
-  ],
-};
