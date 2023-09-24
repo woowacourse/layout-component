ae50752c655070e16b8cc496f015ac51b780b77e
chsua <csj1919@naver.com>
2023-09-24 16:34:05 +0900

    test: flex 인자 수정할 수 있도록 코드 수정

--- src/stories/Flex.stories.tsx
+++ src/stories/Flex.stories.tsx
@@ -1,43 +1,46 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import React from "react";
-import Flex from "../Flex";
+import Flex from "../lib/Flex";
 
 const meta: Meta<typeof Flex> = {
   component: Flex,
+  args: {
+    direction: "row",
+    justify: "center",
+    align: "center",
+    flexWrap: "wrap",
+    gap: 1,
+  },
+  argTypes: {
+    gap: {
+      description: "간격 속성을 정합니다.",
+      control: {
+        type: "range",
+        min: 0,
+        max: 10,
+      },
+    },
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof Flex>;
 
-export const Column: Story = {
-  render: () => (
-    <Flex direction="column" gap={10}>
-      {new Array(5).fill(0).map((_) => {
+export const Default: Story = {
+  render: (args) => (
+    <Flex {...args}>
+      {new Array(10).fill(0).map((_, index) => {
         return (
           <div
             style={{
-              backgroundColor: "royalblue",
-              height: "100px",
+              backgroundColor: `#35${index}88${index}`,
+              height: "55px",
+              width: "35px",
+              color: "white",
             }}
-          />
-        );
-      })}
-    </Flex>
-  ),
-};
-
-export const Rows: Story = {
-  render: () => (
-    <Flex direction="row" justify="flex-end" gap={10} flexWrap="wrap">
-      {new Array(10).fill(0).map((_) => {
-        return (
-          <div
-            style={{
-              backgroundColor: "royalblue",
-              height: "100px",
-              width: "100px",
-            }}
-          />
+          >
+            {index}
+          </div>
         );
       })}
     </Flex>
