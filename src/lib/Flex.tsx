62060093feb5c2cb29780fd095f6341662e6236c
woo-jk <201702042@o.cnu.ac.kr>
2023-09-25 01:42:08 +0900

    refactor: Flex 컴포넌트 요소들 기본값 설정

--- src/lib/Flex.tsx
+++ src/lib/Flex.tsx
@@ -1,18 +1,26 @@
 import { CSSProperties, HTMLAttributes, PropsWithChildren } from "react";
 import { CssNumberValue } from "../types";
 
-type CommonAlignmentValues = "center" | "start" | "end" | "flex-start" | "flex-end";
+type CommonAlignmentValues = "center" | "start" | "end" | "flex-start" | "flex-end" | "stretch";
 
 type Props = {
   direction?: "row" | "column" | "row-reverse" | "column-reverse";
-  justify?: CommonAlignmentValues | "space-between" | "space-around" | "space-evenly" | "stretch";
+  justify?: CommonAlignmentValues | "space-between" | "space-around" | "space-evenly";
   align?: CommonAlignmentValues;
 
   gap?: CssNumberValue;
   wrap?: "wrap" | "nowrap" | "wrap-reverse";
 } & HTMLAttributes<HTMLDivElement>;
 
-const Flex = ({ direction, justify, align, gap = 0, wrap, children, ...attributes }: PropsWithChildren<Props>) => {
+const Flex = ({
+  direction = "row",
+  justify = "flex-start",
+  align = "stretch",
+  wrap = "nowrap",
+  gap = 0,
+  children,
+  ...attributes
+}: PropsWithChildren<Props>) => {
   const defaultStyle: CSSProperties = {
     display: "flex",
 
