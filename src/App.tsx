a78dbb804acbea424360b6291166ef07fc0bc256
Jungwoo <evencoding@gmail.com>
2023-09-24 23:47:37 +0900

    feat: 사용예시

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import TabLayout from './lib/components/TabLayout';
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div className='flex h-screen items-center justify-center'>
+      <div className='h-52 w-52'>
+        <TabLayout tabs={['Tab 1', 'Tab 2', 'Tab 3']} tabPosition='top' activeButtonColor='#0067A3'>
+          <div className='flex h-full items-center justify-center rounded-lg border p-6 shadow-lg'>
+            Content1
+          </div>
+          <div className='flex h-full items-center justify-center rounded-lg border p-6 shadow-lg'>
+            Content2
+          </div>
+          <div className='flex h-full items-center justify-center rounded-lg border p-6 shadow-lg'>
+            Content3
+          </div>
+        </TabLayout>
+      </div>
+    </div>
+  );
 }
 
 export default App;
