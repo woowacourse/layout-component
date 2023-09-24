756b8157a7ae9bfac11fe2300e43afa9ffd62f0b
turtle601 <turtle601@naver.com>
2023-09-24 14:06:41 +0900

    refactor: Container 컴포넌트에 Flex 컴포넌트 적용
    화면 중앙에서 minWidth, maxWidth 가 적용되도록 구현

--- src/Container.tsx
+++ src/Container.tsx
@@ -1,18 +1,53 @@
-import { CSSProperties } from 'react';
+import React, {
+  Children,
+  cloneElement,
+  CSSProperties,
+  ElementType,
+  isValidElement,
+} from 'react';
+import { Props } from './@type/props.type';
+import Flex from './Flex';
 interface ContainerProps {
   minWidth?: CSSProperties['minWidth'];
   maxWidth?: CSSProperties['maxWidth'];
-  children: React.ReactNode;
 }
 
-const defaultStyle: CSSProperties = {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
+const Container = <T extends ElementType = 'div'>({
+  as,
+  minWidth,
+  maxWidth,
+  style,
+  children,
+  ...rest
+}: Props<T, ContainerProps>) => {
+  const Element = as || 'div';
+
+  const styledElement: CSSProperties = {
+    ...style,
+    width: '100%',
+  };
+
+  const styledChildren = Children.map(children, (child) => {
+    if (!isValidElement<Record<'style', {}>>(child)) {
+      throw new Error('자식 요소가 React.child가 아닙니다!!');
+    }
+
+    return cloneElement(child, {
+      style: { ...child.props.style, minWidth, maxWidth },
+    });
+  });
+
+  return (
+    <Flex
+      as={Element}
+      direction="column"
+      align="center"
+      style={{ ...styledElement }}
+      {...rest}
+    >
+      {styledChildren}
+    </Flex>
+  );
 };
-
-function Container({ minWidth, maxWidth, children }: ContainerProps) {
-  return <div style={{ ...defaultStyle, minWidth, maxWidth }}>{children}</div>;
-}
 
 export default Container;
