29de9ed53b61e549ce2e03ff9db75a0eaf0372ec
jeongwusi <jeongwusi@gmail.com>
2023-09-24 21:09:55 +0900

    ♻️ refactor: 스토리북 파일에 컴포넌트에 대한 설명 추가

--- src/TabLayout/TabLayout.stories.tsx
+++ src/TabLayout/TabLayout.stories.tsx
@@ -4,13 +4,21 @@ import TabLayout from "./TabLayout";
 const meta = {
   title: "TabLayout",
   component: TabLayout,
+  parameters: {
+    docs: {
+      description: {
+        component:
+          "TabLayout 컴포넌트는 여러 탭 간에 콘텐츠를 전환하고 사용자에게 편리한 탭 인터페이스를 제공할 수 있습니다.",
+      },
+    },
+  },
 } satisfies Meta<typeof TabLayout>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const Example: Story = {
   args: {
     tabs: ["Tab 1", "Tab 2", "Tab 3"],
     children: [
