e82ffbfcddb6aa76f646a3f73ef65553f44e8bde
2yunseong <dbsdltjd3701@naver.com>
2023-09-24 17:33:51 +0900

    build: 빌드 파일 추가

--- dist/esm/index.js
+++ dist/esm/index.js
@@ -3,9 +3,11 @@ import Grid from './components/Grid';
 import GridItem from './components/GridItem';
 import Flex from './components/Flex';
 import FlexItem from './components/FlexItem';
+import TabLayout from './components/TabLayout';
 export { Container };
 export { Grid };
 export { GridItem };
 export { Flex };
 export { FlexItem };
+export { TabLayout };
 //# sourceMappingURL=index.js.map
