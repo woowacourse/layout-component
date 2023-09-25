5884c2b7b91863300b8e9e1731c45c7219f8a64b
inyeong-kang <tomatorkd@naver.com>
2023-09-26 00:18:51 +0900

    chore: npm 배포를 위한 컴포넌트의 경로 추가

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -2,3 +2,4 @@ export { default as Button } from './Button';
 export { default as Container } from './Container';
 export { default as Flex } from './Flex';
 export { default as Grid } from './Grid';
+export { default as SplitPane } from './SplitPane';
