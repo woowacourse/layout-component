55ef835040a5b26638c327f824e4ce2f3e9936ac
chsua <csj1919@naver.com>
2023-09-26 00:37:12 +0900

    feat: 배포를 위해 index에 tabLayout 컴포넌트 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import Container from "./lib/Container";
 import Flex from "./lib/Flex";
 import Grid from "./lib/Grid";
+import TabLayout from "./lib/TabLayout";
 
-export { Container, Flex, Grid };
+export { Container, Flex, Grid, TabLayout };
