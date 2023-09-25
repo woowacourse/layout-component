1fa6f0b6d0e31c247e83ad72627ce461bf871d0a
Dain Lee <feb.dain@gmail.com>
2023-09-26 06:59:24 +0900

    refactor: children의 타입 변경

--- src/lib/common/types/common.ts
+++ src/lib/common/types/common.ts
@@ -1,10 +1,8 @@
-import type { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react';
+import type { ComponentPropsWithoutRef, ElementType, PropsWithChildren } from 'react';
 
-export interface CommonProps extends ComponentPropsWithoutRef<ElementType> {
+export interface CommonProps extends ComponentPropsWithoutRef<ElementType>, PropsWithChildren {
   /** 태그 변경 가능
    * @default div
    */
   tag?: ElementType;
-  /** 안의 내용물 변경 가능 */
-  children?: ReactNode;
 }
