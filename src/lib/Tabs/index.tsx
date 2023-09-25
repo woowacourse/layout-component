7ca42451930bf9ed91b867b85ff81229004159c5
semnil5202 <semnil5202@gmail.com>
2023-09-25 22:25:50 +0900

    feat: 탭 박스 높이 설정 및 simple 탭 모드 구현

--- src/lib/Tabs/index.tsx
+++ src/lib/Tabs/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 type SizeType = number | string;
 
@@ -11,10 +11,17 @@ interface Props {
   children: React.ReactNode;
   width?: SizeType;
   height?: SizeType;
-  simpleContent?: boolean;
+  $tabBoxHeight?: SizeType;
+  $simpleTab?: boolean;
 }
 
-const Tabs = ({ width = 400, height = 400, children }: Props) => {
+const Tabs = ({
+  width = 400,
+  height = 400,
+  $simpleTab = false,
+  $tabBoxHeight = 40,
+  children,
+}: Props) => {
   const childrenList = React.Children.toArray(
     children
   ) as React.ReactElement<TabProps>[];
@@ -23,15 +30,18 @@ const Tabs = ({ width = 400, height = 400, children }: Props) => {
 
   return (
     <Wrapper width={width}>
-      <TabBoxWrapper>
+      <TabBoxWrapper $simpleTab={$simpleTab} $tabBoxHeight={$tabBoxHeight}>
         {childrenList.map(
           (children, idx) =>
             children && (
               <TabBox
-                key={children.props.label || idx + 1}
+                key={`${children.props.label}, ${idx + 1}`}
+                width={width}
+                $childrenLength={childrenList.length}
+                $simpleTab={$simpleTab}
                 onClick={() => setPos(idx)}
               >
-                {children.props.label || idx + 1}
+                {!$simpleTab && (children.props.label || idx + 1)}
               </TabBox>
             )
         )}
@@ -40,7 +50,7 @@ const Tabs = ({ width = 400, height = 400, children }: Props) => {
       <TabSectionWrapper
         width={width}
         height={height}
-        childrenLength={childrenList.length}
+        $childrenLength={childrenList.length}
         pos={pos}
       >
         {children}
@@ -60,20 +70,26 @@ const setTabSectionWrapperWidth = (width: SizeType, childrenLength: number) =>
     ? `${width * childrenLength}px`
     : `calc(${width} * ${childrenLength})`;
 
+const setTabBoxWidth = (width: SizeType, childrenLength: number) =>
+  typeof width === 'number'
+    ? `${width / childrenLength}px`
+    : `calc(${width} / ${childrenLength})`;
+
 const Wrapper = styled.div<{ width: SizeType }>`
   width: ${({ width }) => translateValue(width)};
   overflow: hidden;
+  margin: 0 auto;
 `;
 
 const TabSectionWrapper = styled.div<{
   width: SizeType;
   height: SizeType;
-  childrenLength: number;
+  $childrenLength: number;
   pos: number;
 }>`
   display: flex;
-  width: ${({ width, childrenLength }) =>
-    setTabSectionWrapperWidth(width, childrenLength)};
+  width: ${({ width, $childrenLength }) =>
+    setTabSectionWrapperWidth(width, $childrenLength)};
   height: ${({ height }) => translateValue(height)};
   transform: translateX(${({ width, pos }) => translatePosition(width, pos)});
   transition: 0.3s ease all;
@@ -83,12 +99,62 @@ const TabSectionWrapper = styled.div<{
   }
 `;
 
-const TabBoxWrapper = styled.div`
+const TabBoxWrapper = styled.div<{
+  $simpleTab: boolean;
+  $tabBoxHeight: SizeType;
+}>`
   display: flex;
+  align-items: center;
+  height: ${({ $tabBoxHeight }) => translateValue($tabBoxHeight)};
 
-  background-color: red;
+  overflow: auto;
+
+  background-color: #959595;
+
+  ${({ $simpleTab }) =>
+    $simpleTab &&
+    css`
+      justify-content: center;
+    `}
 `;
 
-const TabBox = styled.button``;
+const TabBox = styled.button<{
+  width: SizeType;
+  $childrenLength: number;
+  $simpleTab: boolean;
+}>`
+  width: ${({ width, $childrenLength }) =>
+    setTabBoxWidth(width, $childrenLength)};
+  height: inherit;
+  padding: 0.2rem 1rem;
+
+  &:focus {
+    border-bottom: 2px solid #316fc4;
+  }
+
+  ${({ $simpleTab }) =>
+    $simpleTab &&
+    css`
+      padding: 0;
+      width: 24px;
+      height: 24px;
+      border-radius: 50%;
+
+      &:focus {
+        border-bottom: 0;
+        background-color: #316fc4;
+      }
+    `}
+
+  ${({ $simpleTab }) =>
+    $simpleTab &&
+    css`
+      margin-right: 12px;
+
+      &:last-of-type {
+        margin-right: 0;
+      }
+    `}
+`;
 
 export default Tabs;
