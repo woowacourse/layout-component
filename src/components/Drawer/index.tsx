96c7038be7a40827c65cbc267ae3818caca7ea66
Youngjin Park <youngjin988@gmail.com>
2023-09-26 13:52:51 +0900

    feat: `Drawer` 컴포넌트 추가

---
+++ src/components/Drawer/index.tsx
@@ -0,0 +1,85 @@
+import React, { PropsWithChildren } from 'react';
+import styled, { css } from 'styled-components';
+
+export type Anchor = 'left' | 'right' | 'top' | 'bottom';
+
+type Props = {
+  anchor: Anchor;
+  open: boolean;
+  onClose: Function;
+};
+
+const Drawer = ({ children, anchor, open, onClose }: PropsWithChildren<Props>) => {
+  return (
+    <>
+      {open && <Backdrop onClick={() => onClose()} open={open} />}
+      <StyledDrawer anchor={anchor} open={open}>
+        {children}
+      </StyledDrawer>
+    </>
+  );
+};
+
+export default Drawer;
+
+const Backdrop = styled.div<Pick<Props, 'open'>>`
+  display: ${({ open }) => (open ? 'block' : 'none')};
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  padding: 0;
+  margin: 0;
+  background: rgba(0, 0, 0, 0.35);
+`;
+
+const StyledDrawer = styled.div<Omit<Props, 'onClose'>>`
+  position: fixed;
+  transition: transform 300ms;
+  background-color: white;
+
+  ${({ anchor, open }) => {
+    const commonLeftAndRightStyles = css`
+      top: 0;
+      height: 100vh;
+      width: 15%;
+    `;
+
+    const commonUpAndBottomStyles = css`
+      left: 0;
+      right: 0;
+      height: 15%;
+      width: 100vw;
+    `;
+
+    switch (anchor) {
+      case 'left':
+        return css`
+          ${commonLeftAndRightStyles}
+          left: 0;
+          transform: ${open ? 'translateX(0)' : 'translateX(-100%)'};
+        `;
+      case 'right':
+        return css`
+          ${commonLeftAndRightStyles}
+          right: 0;
+          transform: ${open ? 'translateX(0)' : 'translateX(100%)'};
+        `;
+      case 'bottom':
+        return css`
+          ${commonUpAndBottomStyles}
+          bottom: 0;
+          transform: ${open ? 'translateY(0)' : 'translateY(100%)'};
+        `;
+      case 'top':
+        return css`
+          ${commonUpAndBottomStyles}
+          top: 0;
+          transform: ${open ? 'translateY(0)' : 'translateY(-100%)'};
+        `;
+      default:
+        return;
+    }
+  }};
+`;
