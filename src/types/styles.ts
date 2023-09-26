aaa5861aaa63672ee306ebf166fd41f01e21539d
SangwonKang <72087183+ksone02@users.noreply.github.com>
2023-09-26 15:58:13 +0900

    refactor: basic style props 기존 CSSProperties 활용

--- src/types/styles.ts
+++ src/types/styles.ts
@@ -1,17 +1,20 @@
+import { CSSProperties } from 'react';
+
 export type StyleValue = number | string | undefined;
 
-export type DirectionStyle = {
-  top?: StyleValue;
-  left?: StyleValue;
-  right?: StyleValue;
-  bottom?: StyleValue;
-};
+export type DirectionStyle = Pick<
+  CSSProperties,
+  'top' | 'right' | 'bottom' | 'left'
+>;
 
-export type ElementBasicStyleType = {
-  maxWidth?: StyleValue;
-  minWidth?: StyleValue;
+export type ElementBasicStyleType = Pick<
+  CSSProperties,
+  'maxWidth' | 'minWidth' | 'margin'
+> & {
   padding?: StyleValue | DirectionStyle;
   margin?: StyleValue | DirectionStyle;
 };
 
 export type StyleUnit = 'px' | 'rem' | 'em' | '%' | 'vh' | 'vw';
+
+export type EasingType = 'ease-in-out' | 'ease-out' | 'ease-in' | 'ease';
