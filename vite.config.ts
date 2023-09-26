7a8ebc88a56f2e6793ac935ffc99c2ababcc57a0
Dain Lee <feb.dain@gmail.com>
2023-09-26 11:05:48 +0900

    fix: cra 배포 오류 개선

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,28 @@
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), dts(), tsconfigPaths()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'react-layout-box',
+      fileName: (format) => `react-layout-box.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        globals: {
+          react: 'React',
+        },
+      },
+    },
+    commonjsOptions: {
+      esmExternals: ['react'],
+    },
+  },
 });
