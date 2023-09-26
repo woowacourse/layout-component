611cc1c5615923dde95345dfd42547b2c1a3998b
SangwonKang <72087183+ksone02@users.noreply.github.com>
2023-09-26 15:58:50 +0900

    feat: ParallaxItem export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import Container from './Container/Container';
 import Flex from './Flex/Flex';
 import Grid from './Grid/Grid';
+import ParallaxItem from './ParallaxItem/ParallaxItem';
 
-export { Container, Flex, Grid };
+export { Container, Flex, Grid, ParallaxItem };
