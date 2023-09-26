20feec6095ef460c7259fffd9aa9b7c0a9361e14
afds4567 <33995840+afds4567@users.noreply.github.com>
2023-09-26 14:00:22 +0900

    refactor: Masonry 배포용 수정

--- layout-component/src/main.tsx
+++ layout-component/src/main.tsx
@@ -1,21 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Grid } from "./lib/components/Grid";
+import { Masonry } from "./lib/components/Masonry";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Grid
-      rows={2}
-      columns={2}
-      gap={10}
-      width="100vw"
-      height="100vh"
-      backgroundColor="blue"
-    >
-      <div>Item 1</div>
-      <div>Item 2</div>
-      <div>Item 3</div>
-      <div>Item 4</div>
-    </Grid>
+    <Masonry columns={4}>
+      <div style={{ background: "red", height: "200px" }}>1</div>
+      <div style={{ background: "yellow", height: "280px" }}>2</div>
+      <div style={{ background: "blue", height: "100px" }}>3</div>
+      <div style={{ background: "green", height: "340px" }}>4</div>
+      <div style={{ background: "red", height: "200px" }}>5</div>
+      <div style={{ background: "yellow", height: "260px" }}>6</div>
+      <div style={{ background: "blue", height: "120px" }}>7</div>
+      <div style={{ background: "green", height: "40px" }}>8</div>
+    </Masonry>
   </React.StrictMode>
 );
