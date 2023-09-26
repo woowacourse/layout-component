e5fb1fbc89b3d5f274011befb3349ceb156a5f4d
Youngjin Park <youngjin988@gmail.com>
2023-09-26 13:52:39 +0900

    feat: `Drawer` 스토리 추가

--- src/components/Drawer/Drawer.stories.tsx
+++ src/components/Drawer/Drawer.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Meta, StoryObj } from '@storybook/react';
-import Drawer from './index';
+import Drawer, { Anchor } from './index';
 
 const meta = {
   title: 'Drawer',
@@ -13,6 +13,37 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Playground: Story = {};
+export const Playground: Story = {
+  render: () => {
+    const [state, setState] = React.useState({
+      top: false,
+      left: false,
+      bottom: false,
+      right: false,
+    });
+
+    const toggleDrawer = (anchor: Anchor, open: boolean) => {
+      setState({ ...state, [anchor]: open });
+    };
+
+    return (
+      <>
+        {(['left', 'right', 'top', 'bottom'] as const).map((anchor) => (
+          <React.Fragment key={anchor}>
+            <button onClick={() => toggleDrawer(anchor, true)}>{anchor}</button>
+            <Drawer
+              anchor={anchor}
+              open={state[anchor]}
+              onClose={() => toggleDrawer(anchor, false)}
+            >
+              {/* {list(anchor)} */}
+              <div>asd</div>
+            </Drawer>
+          </React.Fragment>
+        ))}
+      </>
+    );
+  },
+};
 
 export const Variants: Story = {};
