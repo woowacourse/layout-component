552c1ebf709ad6641e76cda5d298e4844cf89164
Leejin-Yang <ejin1996@gmail.com>
2023-09-26 15:19:41 +0900

    feat: Drawer 컴포넌트 작성

---
+++ src/drawer/drawer.tsx
@@ -0,0 +1,47 @@
+import type { ReactElement } from 'react';
+import { createPortal } from 'react-dom';
+
+import DrawerContent from './drawer-content';
+import DrawerBackdrop from './drawer-backdrop';
+import { DrawerContext } from './drawer-context';
+import { useDrawer } from './hooks';
+import { Placement } from './drawer.type';
+
+type DrawerProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  animation?: boolean;
+  placement?: Placement;
+  portalElement?: Element;
+  children: ReactElement;
+};
+
+const Drawer = ({
+  isOpen,
+  onClose,
+  animation = true,
+  placement = 'left',
+  portalElement = document.body,
+  children,
+}: DrawerProps) => {
+  const { isCloseReady, closeDrawer } = useDrawer({ animation, onClose });
+
+  const drawerConfig = {
+    animation,
+    isCloseReady,
+    placement,
+    onClose: closeDrawer,
+  };
+
+  return createPortal(
+    <DrawerContext.Provider value={drawerConfig}>
+      {isOpen ? children : null}
+    </DrawerContext.Provider>,
+    portalElement
+  );
+};
+
+Drawer.Backdrop = DrawerBackdrop;
+Drawer.Content = DrawerContent;
+
+export default Drawer;
