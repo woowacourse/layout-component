34028c50175926a22c268a9d0d452f3f108a9c36
Leejin-Yang <ejin1996@gmail.com>
2023-09-26 15:20:00 +0900

    feat: DrawerBackdrop 컴포넌트 작성

---
+++ src/drawer/drawer-backdrop.tsx
@@ -0,0 +1,18 @@
+import { useDrawerContext } from './drawer-context';
+
+import styles from './drawer-backdrop.module.css';
+
+const DrawerBackdrop = () => {
+  const { onClose } = useDrawerContext();
+
+  return (
+    <button
+      type='button'
+      className={styles.backdrop}
+      onClick={onClose}
+      aria-hidden
+    />
+  );
+};
+
+export default DrawerBackdrop;
