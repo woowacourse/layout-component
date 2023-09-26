49aeae8a1b896db5fe978290ba7c51bc7569d442
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-09-26 17:22:04 +0900

    chore: vite 설정 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,23 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import path from 'path';
+import react from '@vitejs/plugin-react-swc';
 import dts from 'vite-plugin-dts';
-import pkg from './package.json';
 
-const makeExternalPredicate = (externalArr: string[]) => {
-  if (externalArr.length === 0) {
-    return () => false;
-  }
-
-  const pattern = new RegExp(`^(${externalArr.join('|')})($|/)`);
-
-  return (id: string) => pattern.test(id);
-};
-
-const externals = makeExternalPredicate(Object.keys(pkg.peerDependencies));
-
-// https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/lib/index.ts'),
-      name: 'index',
-      fileName: 'index',
+      entry: resolve(__dirname, 'src/lib/index.ts'),
+      name: 'react-aaker-layout-components',
+      fileName: (format) => `react-aaker-layout-components.${format}.js`,
     },
     rollupOptions: {
-      external: externals,
+      external: ['react', 'react-dom'],
       output: {
         globals: {
           react: 'React',
-          '@emotion/react/jsx-runtime': 'jsxRuntime',
-          '@emotion/react': 'react',
-          '@emotion/styled': 'styled',
         },
       },
     },
-    commonjsOptions: {
-      esmExternals: ['react'],
-    },
   },
-  plugins: [react(), tsconfigPaths(), dts()],
+  plugins: [react(), dts({ rollupTypes: true })],
 });
