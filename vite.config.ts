26882a26f29dae80bd29401bf534bc0cde43b73e
Dain Lee <feb.dain@gmail.com>
2023-09-26 14:14:23 +0900

    fix: 배포 오류 개선

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import react from '@vitejs/plugin-react';
-import path from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -7,22 +6,4 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), dts(), tsconfigPaths()],
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'react-layout-box',
-      fileName: (format) => `react-layout-box.${format}.js`,
-    },
-    rollupOptions: {
-      external: ['react'],
-      output: {
-        globals: {
-          react: 'React',
-        },
-      },
-    },
-    commonjsOptions: {
-      esmExternals: ['react'],
-    },
-  },
 });
