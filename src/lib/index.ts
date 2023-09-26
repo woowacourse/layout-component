90d135ee73ec5d89eec42dce0e56187dc6419b15
regularPark <srk99279@naver.com>
2023-09-26 14:07:16 +0900

    style: import 문 수정

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,5 @@
 export { Container } from './Container';
 export { Flex } from './Flex';
 export { Grid } from './Grid';
-export { Drawer } from './Drawer/index';
+export { Drawer } from './Drawer';
 export { useDrawer } from './Drawer/hook/useDrawer';
