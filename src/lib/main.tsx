c078eb34cbf15772e2c1b31828751b2d968b32cb
GC-Park <kuyt3122@naver.com>
2023-09-26 15:12:14 +0900

    refactor: 변경된 Drawer 속성들 값에 맞게 main.tsx 변경

--- src/lib/main.tsx
+++ src/lib/main.tsx
@@ -11,6 +11,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <Container />
     <Flex />
     <Grid />
-    <Drawer direction={'right'} isShow={false} size={'xs'} closeSheet={() => {}} />
+    <Drawer direction={'right'} isShow={false} closeDrawer={() => {}} />
   </React.StrictMode>,
 )
