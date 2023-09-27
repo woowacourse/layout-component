6c0c8dd77ee94b998c259aba6c2ef8b321fd88d2
Gilpop8663 <wolfye@naver.com>
2023-09-27 15:36:13 +0900

    docs: 공통 스타일 Props에 대해해jsDoc 작성

--- src/lib/types/common.ts
+++ src/lib/types/common.ts
@@ -10,7 +10,13 @@ import { CommonStyleProps } from '../style/common';
 interface Common<T extends ElementType>
   extends CommonStyleProps,
     PropsWithChildren {
+  /**
+   * HTML 태그를 문자열로 입력해 원하는 HTML 태그로 사용할 수 있습니다.
+   */
   as?: T;
+  /**
+   * 디테일 한 CSS 속성을 지정해야 할 경우 직접 CSS를 입력할 수 있습니다.
+   */
   css?: CSSProperties;
 }
 
