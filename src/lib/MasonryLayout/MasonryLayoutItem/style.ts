27ce5312c4aa230c3cdeb7dbf6fdac9450a6e2e0
gilpop8663 <wolfye@naver.com>
2023-09-27 23:23:36 +0900

    fix: 반응형에서 의도한대로 동작하지 않는 코드 수정

--- src/lib/MasonryLayout/MasonryLayoutItem/style.ts
+++ src/lib/MasonryLayout/MasonryLayoutItem/style.ts
@@ -17,20 +17,16 @@ interface ComponentProps {
 }
 
 const getPaddingBottom = ({
-  gap,
-  rowGap = '0',
   responsiveGap,
   responsiveRowGap,
 }: {
-  gap?: string;
-  rowGap?: string;
   responsiveGap?: string;
   responsiveRowGap?: string;
 }) => {
-  const defaultGap = gap ? gap : rowGap;
   const responseColumnGap = responsiveGap ? responsiveGap : responsiveRowGap;
+
   if (!responseColumnGap) {
-    return defaultGap;
+    return;
   }
 
   return responseColumnGap;
@@ -38,55 +34,45 @@ const getPaddingBottom = ({
 
 export const Component = styled.div<ComponentProps>`
   width: 100%;
+  height: max-content;
   padding-bottom: ${({ gap, rowGap = '0' }) => (gap ? gap : rowGap)};
   grid-row-end: ${({ height }) => `span ${height}`};
-  height: max-content;
 
   @media (min-width: 640px) {
-    padding-bottom: ${({ gap, rowGap, smallGap, smallRowGap }) =>
+    padding-bottom: ${({ smallGap, smallRowGap }) =>
       getPaddingBottom({
-        gap,
-        rowGap,
         responsiveGap: smallGap,
         responsiveRowGap: smallRowGap,
       })};
   }
 
   @media (min-width: 768px) {
-    padding-bottom: ${({ gap, rowGap, mediumGap, mediumRowGap }) =>
+    padding-bottom: ${({ mediumGap, mediumRowGap }) =>
       getPaddingBottom({
-        gap,
-        rowGap,
         responsiveGap: mediumGap,
         responsiveRowGap: mediumRowGap,
       })};
   }
 
   @media (min-width: 1024px) {
-    padding-bottom: ${({ gap, rowGap, largeGap, largeRowGap }) =>
+    padding-bottom: ${({ largeGap, largeRowGap }) =>
       getPaddingBottom({
-        gap,
-        rowGap,
         responsiveGap: largeGap,
         responsiveRowGap: largeRowGap,
       })};
   }
 
   @media (min-width: 1280px) {
-    padding-bottom: ${({ gap, rowGap, xLargeGap, xLargeRowGap }) =>
+    padding-bottom: ${({ xLargeGap, xLargeRowGap }) =>
       getPaddingBottom({
-        gap,
-        rowGap,
         responsiveGap: xLargeGap,
         responsiveRowGap: xLargeRowGap,
       })};
   }
 
   @media (min-width: 1536px) {
-    padding-bottom: ${({ gap, rowGap, doubleXLargeGap, doubleXLargeRowGap }) =>
+    padding-bottom: ${({ doubleXLargeGap, doubleXLargeRowGap }) =>
       getPaddingBottom({
-        gap,
-        rowGap,
         responsiveGap: doubleXLargeGap,
         responsiveRowGap: doubleXLargeRowGap,
       })};
