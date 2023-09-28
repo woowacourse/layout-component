9a1505e370091141e3096199543860f714e704d1
chex <chex1004@gmail.com>
2023-09-29 01:38:09 +0900

    chore: 불필요한 코드 삭제 및 버전 업데이트

--- src/GlobalStyle.ts
+++ src/GlobalStyle.ts
@@ -10,40 +10,6 @@ const GlobalStyle = createGlobalStyle`
   html {
     font-size: 62.5%; /* 1rem이 10px로 설정됨 */
   }
-
-  a {
-    color: inherit;
-    text-decoration: none;
-    transition: all 0.3s ease-in;
-  }
-
-  ul {
-    list-style: none;
-  }
-
-  a:hover {
-    transform: scale(1.02);
-  }
-
-  a:active {
-    animation: shrink-grow 0.3s ease;
-    transform-origin: center;
-  }
-
-  @keyframes shrink-grow {
-    0% {
-    transform: scale(1);
-    }
-    25% {
-      transform: scale(0.98);
-    }
-    50% {
-      transform: scale(0.99);
-    }
-    100% {
-      transform: scale(1);
-    }
-  }
 `;
 
 export default GlobalStyle;
