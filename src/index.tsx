1ae52e21781ac31cf3c8a9337a3645ef2f1d5a50
Ashley Heo <ashleysyheo@gmail.com>
2023-09-30 20:38:24 +0900

    feat: Masonry 컴포넌트 export하기

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,6 @@ import LayoutProvider from '@/LayoutProvider';
 import Container from '@components/Container';
 import Flex from '@components/Flex';
 import Grid from '@components/Grid';
+import Masonry from '@components/Masonry';
 
-export { LayoutProvider, Container, Flex, Grid };
+export { LayoutProvider, Container, Flex, Grid, Masonry };
