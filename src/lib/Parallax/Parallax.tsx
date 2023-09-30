9fd9d9d70922b7dfea70b7d79762f3b4d4860a93
Dohyeon Lee <dlehgus1110@naver.com>
2023-09-30 22:00:37 +0900

    refactor: Parallax 컴포넌트 스타일드 컴포넌트 마이그레이션

--- src/lib/Parallax/Parallax.tsx
+++ src/lib/Parallax/Parallax.tsx
@@ -1,48 +1,46 @@
 import { PropsWithChildren } from 'react';
+import styled from 'styled-components';
 
 type ParallaxProps = {
-  image?: string;
-  height?: React.CSSProperties['height'];
+  $image?: string;
+  $height?: React.CSSProperties['height'];
 };
 
-const Parallax = ({ image, height = '100vh', children }: PropsWithChildren<ParallaxProps>) => {
-  const containerStyles: React.CSSProperties = {
-    height,
-    overflowX: 'hidden',
-    overflowY: 'auto',
-    background: image && `no-repeat center url(${image})`,
-    backgroundSize: 'cover',
-    backgroundAttachment: 'fixed',
-    transformStyle: 'preserve-3d',
-    perspective: '1px',
-    position: 'relative',
-  };
-
-  return <div style={containerStyles}>{children}</div>;
+const Parallax = ({ children, ...props }: PropsWithChildren<ParallaxProps>) => {
+  return <ParallaxContainer {...props}>{children}</ParallaxContainer>;
 };
 
 type ParallaxItemProps = {
-  speed: number;
-  image?: string;
-  width?: React.CSSProperties['width'];
+  $speed: number;
+  $image?: string;
+  $width?: React.CSSProperties['width'];
 };
 
-const Item = ({ speed, image, width = '100%', children }: PropsWithChildren<ParallaxItemProps>) => {
-  const translateZ = speed * -1;
-  const scale = speed + 1;
-
-  const parallaxStyles: React.CSSProperties = {
-    transform: `translateZ(${translateZ}px) scale(${scale})`,
-    background: image ? `url(${image}) no-repeat center` : '',
-    width: '100%',
-    aspectRatio: '1/2',
-    position: 'absolute',
-    inset: '0',
-  };
-
-  return <div style={parallaxStyles}>{children}</div>;
+const Item = ({ children, ...props }: PropsWithChildren<ParallaxItemProps>) => {
+  return <ParallaxItem {...props}>{children}</ParallaxItem>;
 };
 
 Parallax.Item = Item;
 
 export default Parallax;
+
+const ParallaxContainer = styled.div<ParallaxProps>`
+  height: ${({ $height }) => $height || '100vh'};
+  overflow-x: hidden;
+  overflow-y: auto;
+  background: ${({ $image }) => $image && `no-repeat center url(${$image})`};
+  background-size: cover;
+  background-attachment: fixed;
+  transform-style: preserve-3d;
+  perspective: 2px;
+  position: relative;
+`;
+
+const ParallaxItem = styled.div<ParallaxItemProps>`
+  width: ${({ $width }) => $width || '100%'};
+  transform: ${({ $speed }) => `translateZ(${$speed * -2}px) scale(${$speed + 1})`};
+  position: absolute;
+  inset: 0;
+  background: ${({ $image }) => $image && `no-repeat center url(${$image})`};
+  background-size: cover;
+`;
