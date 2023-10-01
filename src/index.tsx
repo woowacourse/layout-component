263268cda4e8a5a89bd5916c24c0de87a1e10e60
hozzijeong <ghwls0905@naver.com>
2023-10-01 17:00:09 +0900

    refactor: index.tsx 수정

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,6 @@
 import Container from './components/layouts/Container';
 import Flex from './components/layouts/Flex';
 import Grid from './components/layouts/Grid';
-import TabList from './components/layouts/Tab/TabList';
-import TabButton from './components/layouts/Tab/TabButton';
-import TabPanel from './components/layouts/Tab/TabPanel';
-import { TabPanels } from './components/layouts/Tab/Tab.styles';
-import useChangeTab from './components/layouts/Tab/hooks/useChangeTab';
+import Tabs from './components/layouts/Tab/Tabs';
 
-export {
-	Container,
-	Flex,
-	Grid,
-	TabList,
-	TabButton,
-	TabPanel,
-	TabPanels,
-	useChangeTab,
-};
+export { Container, Flex, Grid, Tabs };
