45d4d6dfdc1276f8fdcce4fcb0864bdf47140b72
Dohyeon Lee <dlehgus1110@naver.com>
2023-10-01 14:12:43 +0900

    refactor: 스타일드 컴포넌트 -> pure css 코드로 재 변경
    y축 동적 스타일링 시 레이어가 쌓일수록 스크롤 지연 발생

--- src/lib/Parallax/Parallax.tsx
+++ src/lib/Parallax/Parallax.tsx
@@ -1,46 +1,50 @@
-import { PropsWithChildren } from 'react';
-import styled from 'styled-components';
+import { PropsWithChildren, useRef, useState, useEffect } from 'react';
+import { useScrollContext } from './ParallaxContext';
 
 type ParallaxProps = {
-  $image?: string;
-  $height?: React.CSSProperties['height'];
+  image?: string;
+  height?: React.CSSProperties['height'];
+  css?: React.CSSProperties;
 };
 
-const Parallax = ({ children, ...props }: PropsWithChildren<ParallaxProps>) => {
-  return <ParallaxContainer {...props}>{children}</ParallaxContainer>;
+const Parallax = ({ image, height = '100vh', css, children }: PropsWithChildren<ParallaxProps>) => {
+  const containerStyles: React.CSSProperties = {
+    height,
+    overflowX: 'hidden',
+    overflowY: 'hidden',
+    background: image && `no-repeat center url(${image})`,
+    backgroundSize: 'cover',
+    backgroundAttachment: 'fixed',
+    position: 'relative',
+    ...css,
+  };
+
+  return <div style={containerStyles}>{children}</div>;
 };
 
 type ParallaxItemProps = {
-  $speed: number;
-  $image?: string;
-  $width?: React.CSSProperties['width'];
+  speed: number;
+  image?: string;
+  width?: React.CSSProperties['width'];
+  inset?: React.CSSProperties['inset'];
 };
 
-const Item = ({ children, ...props }: PropsWithChildren<ParallaxItemProps>) => {
-  return <ParallaxItem {...props}>{children}</ParallaxItem>;
+const Item = ({ speed, image, width = '100%', inset = 0, children }: PropsWithChildren<ParallaxItemProps>) => {
+  const { scroll } = useScrollContext();
+
+  const parallaxStyles: React.CSSProperties = {
+    willChange: 'transform',
+    transform: `translate3d(0,${(scroll / 20) * speed}px,0)`,
+    background: image ? `url(${image}) no-repeat center` : '',
+    backgroundSize: 'cover',
+    position: 'absolute',
+    width,
+    inset,
+  };
+
+  return <div style={parallaxStyles}>{children}</div>;
 };
 
 Parallax.Item = Item;
 
 export default Parallax;
-
-const ParallaxContainer = styled.div<ParallaxProps>`
-  height: ${({ $height }) => $height || '100vh'};
-  overflow-x: hidden;
-  overflow-y: auto;
-  background: ${({ $image }) => $image && `no-repeat center url(${$image})`};
-  background-size: cover;
-  background-attachment: fixed;
-  transform-style: preserve-3d;
-  perspective: 2px;
-  position: relative;
-`;
-
-const ParallaxItem = styled.div<ParallaxItemProps>`
-  width: ${({ $width }) => $width || '100%'};
-  transform: ${({ $speed }) => `translateZ(${$speed * -2}px) scale(${$speed + 1})`};
-  position: absolute;
-  inset: 0;
-  background: ${({ $image }) => $image && `no-repeat center url(${$image})`};
-  background-size: cover;
-`;
