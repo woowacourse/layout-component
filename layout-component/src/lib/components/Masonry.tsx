2576b3d3e4933b3e09d9242caeb95d890c5b0e8c
afds4567 <33995840+afds4567@users.noreply.github.com>
2023-10-04 19:22:29 +0900

    refactor: gap을 인자로 받도록 수정

--- layout-component/src/lib/components/Masonry.tsx
+++ layout-component/src/lib/components/Masonry.tsx
@@ -9,16 +9,20 @@ import React, {
 
 interface MasonryLayoutProps {
   columns: number;
+  gap: number;
   children: ReactNode;
 }
 
-function distributeChildrenIntoColumns(children: ReactNode, columns: number) {
+function distributeChildrenIntoColumns(
+  children: ReactNode,
+  columns: number,
+  gap: number
+) {
   const columnWrapper = Array.from(
     { length: columns },
     () => [] as (ReactElement | null)[]
   );
   const columnHeights = Array.from({ length: columns }, () => 0);
-  const marginBottom = 16;
 
   Children.forEach(children, (child) => {
     if (!isValidElement(child)) return;
@@ -35,25 +39,31 @@ function distributeChildrenIntoColumns(children: ReactNode, columns: number) {
     const newChild = (
       <elementChild.type
         {...elementChild.props}
-        style={{ ...elementChild.props.style, marginBottom, width: "100%" }}
+        style={{
+          ...elementChild.props.style,
+          marginBottom: gap,
+          width: "100%",
+        }}
       />
     );
 
     columnWrapper[shortestColumnIndex].push(newChild);
-    columnHeights[shortestColumnIndex] += itemHeight + marginBottom;
+    columnHeights[shortestColumnIndex] += itemHeight + gap;
   });
 
   return columnWrapper;
 }
 
-export const Masonry = ({ columns, children }: MasonryLayoutProps) => {
+export const Masonry = ({ columns, children, gap }: MasonryLayoutProps) => {
+  // 여기에 새로운 prop을 추가합니다.
+
   const [columnWrapper, setColumnWrapper] = useState<
     Array<(React.ReactElement | null)[]>
   >([]);
 
   useEffect(() => {
-    setColumnWrapper(distributeChildrenIntoColumns(children, columns));
-  }, [columns, children]);
+    setColumnWrapper(distributeChildrenIntoColumns(children, columns, gap));
+  }, [columns, children, gap]);
 
   return (
     <div style={{ display: "flex" }}>
